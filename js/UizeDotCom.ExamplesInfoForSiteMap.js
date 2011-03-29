bab0024a3372b93bbd8733896b4e309b7c09142c
Chris van Rensburg <uize-git@tomkidding.com>
2011-03-29 14:34:51 -0700

    Announcing that UIZE is now available on GitHub.

--- js/UizeDotCom.ExamplesInfoForSiteMap.js
+++ js/UizeDotCom.ExamplesInfoForSiteMap.js
@@ -1,5 +1,5 @@
 /*
-	UIZE Web Site 2011-03-24
+	UIZE Web Site 2011-03-29
 
 	http://www.uize.com/reference/UizeDotCom.ExamplesInfoForSiteMap.html
 	Available under MIT License or GNU General Public License -- http://www.uize.com/license.html
