9a94d39e9d0b068ee90bc41baeeb7a21c11c2c71
Chris van Rensburg <uize-git@tomkidding.com>
2011-05-05 00:57:43 -0700

    Adding and announcing support for section title aliases in SimpleDoc

--- js/UizeDotCom.ExamplesInfoForSiteMap.js
+++ js/UizeDotCom.ExamplesInfoForSiteMap.js
@@ -1,5 +1,5 @@
 /*
-	UIZE Web Site 2011-04-26
+	UIZE Web Site 2011-05-05
 
 	http://www.uize.com/reference/UizeDotCom.ExamplesInfoForSiteMap.html
 	Available under MIT License or GNU General Public License -- http://www.uize.com/license.html
