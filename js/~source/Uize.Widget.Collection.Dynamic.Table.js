91aac862a77e35d15f7f751e8f6f0c2601441384
Ben Ilegbodu <ben@zazzle.com>
2011-09-29 15:22:46 -0700

    updates to the directional pad so that it will update its UI the way other widgets do.

---
+++ js/~source/Uize.Widget.Collection.Dynamic.Table.js
@@ -0,0 +1,64 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widget.Collection.Dynamic.Table Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2011 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/*ScruncherSettings Mappings="=f" LineCompacting="TRUE"*/
+
+/* Module Meta Data
+	type: Class
+	importance: 4
+	codeCompleteness: 100 
+	testCompleteness: 0
+	docCompleteness: 100 
+*/
+
+/*?
+	Introduction
+		The =Uize.Widget.Collection.Dynamic.Table= class thinly extends =Uize.Widget.Collection.Dynamic= by adding support for handling collections of table rows.
+
+		It is assumed that each collection item is represented in markup by a table row (tr).
+
+		*DEVELOPERS:* `Vinson Chuong`
+*/
+
+Uize.module ({
+	name:'Uize.Widget.Collection.Dynamic.Table',
+	builder:function (_superclass) {
+		/*** Class Constructor ***/
+			var
+				_class = _superclass.subclass (),
+				_classPrototype = _class.prototype
+			;
+
+		/*** Public Instance Methods ***/
+			_classPrototype.processItemTemplate = function (_templateNode) {
+				var _this = this;
+				return function (_input) {
+					var _dummyNode = document.createElement ('DIV');
+					_dummyNode.innerHTML =
+						'<table><tbody>' +
+						_superclass.prototype.processItemTemplate.call (_this, _templateNode) (_input) +
+						'</tbody></table>'
+					;
+					var
+						_nodeList = _dummyNode.firstChild.firstChild.childNodes,
+						_nodes = []
+					;
+					for (var _nodeNo = -1, _nodeListLength = _nodeList.length; ++_nodeNo < _nodeListLength;)
+						_nodes.push (_nodeList[_nodeNo])
+					;
+					return _nodes;
+				};
+			};
+
+		return _class;
+	}
+});
+
