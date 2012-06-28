b75b793dcd3523820ff42ddca4793d687f36d3cd
Chris van Rensburg <uize-git@tomkidding.com>
2012-06-27 21:31:37 -0700

    The Uize base module gets a new Uize.resolveMatcher method that is used by a bunch of other modules.

--- js/UizeDotCom.ExamplesInfoForSiteMap.js
+++ js/UizeDotCom.ExamplesInfoForSiteMap.js
@@ -1,5 +1,5 @@
 /*
-	UIZE Web Site 2012-06-25
+	UIZE Web Site 2012-06-27
 
 	http://www.uize.com/reference/UizeDotCom.ExamplesInfoForSiteMap.html
 	Available under MIT License or GNU General Public License -- http://www.uize.com/license.html
--- js/UizeDotCom.ModulesTree.js
+++ js/UizeDotCom.ModulesTree.js
@@ -1,5 +1,5 @@
 /*
-	UIZE Web Site 2012-06-25
+	UIZE Web Site 2012-06-27
 
 	http://www.uize.com/reference/UizeDotCom.ModulesTree.html
 	Available under MIT License or GNU General Public License -- http://www.uize.com/license.html
