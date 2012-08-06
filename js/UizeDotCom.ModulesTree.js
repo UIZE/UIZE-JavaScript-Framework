a9db4d85fb9f8350976425a8439db34bead7070e
Chris van Rensburg <uize-git@tomkidding.com>
2012-08-06 08:47:38 -0700

    Finalized unit tests for is instance method of Uize.Class and added announcement for new method.

--- js/UizeDotCom.ModulesTree.js
+++ js/UizeDotCom.ModulesTree.js
@@ -1,5 +1,5 @@
 /*
-	UIZE Web Site 2012-08-04
+	UIZE Web Site 2012-08-06
 
 	http://www.uize.com/reference/UizeDotCom.ModulesTree.html
 	Available under MIT License or GNU General Public License -- http://www.uize.com/license.html
