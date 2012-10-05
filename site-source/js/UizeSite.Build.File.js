34496377df70f0c8e85e427566893e10390866c3
Chris van Rensburg <uize-git@tomkidding.com>
2012-10-04 21:59:35 -0700

    Starting to do plumbing for new build approach.

---
+++ site-source/js/UizeSite.Build.File.js
@@ -0,0 +1,42 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : UizeSite.Build.File Namespace
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2012 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Namespace
+	importance: 5
+	codeCompleteness: 0
+	testCompleteness: 0
+	docCompleteness: 2
+*/
+
+/*?
+	Introduction
+		The =UizeSite.Build.File= package provides a method for building any file requested for the UIZE Web site.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'UizeSite.Build.File',
+	required:[
+	],
+	builder:function () {
+		/*** Variables for Scruncher Optimization ***/
+			var _package = function () {};
+
+		/*** Public Static Methods ***/
+			_package.perform = function (_params) {
+			};
+
+		return _package;
+	}
+});
+
