3626f00c7b8ea3dbc2101a699b016e02f947f3b7
Chris van Rensburg <uize-git@tomkidding.com>
2012-12-29 17:55:02 -0800

    Implemented a basic service interface definition for a new store service.

---
+++ site-source/js/Uize.Services.Store.js
@@ -0,0 +1,134 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Services.Store Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2012 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 6
+	codeCompleteness: 0
+	testCompleteness: 0
+	docCompleteness: 2
+*/
+
+/*?
+	Introduction
+		The =Uize.Services.Store= module defines a class for a simple key/value store service.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Services.Store',
+	superclass:'Uize.Service',
+	builder:function (_superclass) {
+		/*** Variables for Scruncher Optimization ***/
+			var
+				_false = false,
+				_true = true
+			;
+
+		/*** Class Constructor ***/
+			var _class = _superclass.subclass ();
+
+		/*** Public Instance Methods ***/
+			_class.declareServiceMethods ({
+				key:{
+					async:_false
+					/*?
+						Instance Methods
+							key
+								Returns the key of the item at the specified index.
+
+								SYNTAX
+								.......................................
+								keySTR = storeInstance.key (paramsOBJ);
+								.......................................
+
+								Params
+									index
+										document...
+					*/
+				},
+				getItem:{
+					async:_false
+					/*?
+						Instance Methods
+							getItem
+								Returns the value for the specified item.
+
+								SYNTAX
+								.................................................
+								itemValueSTR = storeInstance.getItem (paramsOBJ);
+								.................................................
+
+								Params
+									key
+										document...
+					*/
+				},
+				setItem:{
+					async:_false
+					/*?
+						Instance Methods
+							setItem
+								document...
+
+								SYNTAX
+								..................................
+								storeInstance.setItem (paramsOBJ);
+								..................................
+
+								Params
+									key
+										document...
+
+									value
+										document...
+					*/
+				},
+				removeItem:{
+					async:_false
+					/*?
+						Instance Methods
+							removeItem
+								document...
+
+								SYNTAX
+								.....................................
+								storeInstance.removeItem (paramsOBJ);
+								.....................................
+
+								Params
+									key
+										document...
+					*/
+				},
+				clear:{
+					async:_false
+					/*?
+						Instance Methods
+							clear
+								document...
+
+								SYNTAX
+								................................
+								storeInstance.clear (paramsOBJ);
+								................................
+
+								Params
+									The =clear= method has no parameters.
+					*/
+				}
+			});
+
+		return _class;
+	}
+});
+
