2acdb80c8ac69bb000605df599de13e5166f83c1
Chris van Rensburg <uize-git@tomkidding.com>
2012-12-30 20:00:45 -0800

    Started making some stubbing progress with the new store service.

---
+++ site-source/js/Uize.Services.StoreAdapter.BrowserLocalStorage.js
@@ -0,0 +1,62 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Services.StoreAdapter.BrowserLocalStorage Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2012 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 6
+	codeCompleteness: 0
+	testCompleteness: 0
+	docCompleteness: 5
+*/
+
+/*?
+	Introduction
+		The =Uize.Services.StoreAdapter.BrowserLocalStorage= module defines an adapter for the file system service (=Uize.Services.Store=) for the BrowserLocalStorageJS environment.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Services.StoreAdapter.BrowserLocalStorage',
+	superclass:'Uize.Services.StoreAdapter',
+	builder:function (_superclass) {
+		/*** Class Constructor ***/
+			var
+				_class = _superclass.subclass (),
+				_classPrototype = _class.prototype
+			;
+
+		/*** Public Instance Methods ***/
+			_class.instanceMethods ({
+				key:function (_params,_callback) {
+				},
+
+				getItem:function (_params,_callback) {
+				},
+
+				setItem:function (_params,_callback) {
+				},
+
+				removeItem:function (_params,_callback) {
+				},
+
+				clear:function (_params,_callback) {
+				},
+
+				init:function (_params,_callback) {
+					_callback ();
+				}
+			});
+
+		return _class;
+	}
+});
+
