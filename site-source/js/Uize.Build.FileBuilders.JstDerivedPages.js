babd980bfcfb174aade905eca85eff35bae65f28
Chris van Rensburg <uize-git@tomkidding.com>
2013-02-27 23:43:38 -0800

    Added a file builder for files that are built by processing JST files, where the built file must have a corresponding file of the same sub-path in the source folder, and with a .jst suffix.

---
+++ site-source/js/Uize.Build.FileBuilders.JstDerivedPages.js
@@ -0,0 +1,49 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Build.FileBuilders.JstDerivedPages Package
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Package
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Build.FileBuilders.JstDerivedPages= module defines a file builder for pages derived from JST template files.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+
+		Functions defined in the file builder are called as instance methods on an instance of a subclass of the =Uize.Services.FileBuilderAdapter= class, so the functions can access instance methods implemented in this class.
+*/
+
+Uize.module ({
+	name:'Uize.Build.FileBuilders.JstDerivedPages',
+	builder:function () {
+		return {
+			description:'Pages derived from JST template files',
+			urlMatcher:function (_urlParts) {
+				var _pathname = _urlParts.pathname;
+				return (
+					this.isBuiltUrl (_pathname) &&
+					this.fileExists ({path:this.sourceUrlFromBuiltUrl (_pathname) + '.jst'})
+				);
+			},
+			builderInputs:function (_urlParts) {
+				return {jstTemplate:this.memoryUrlFromBuiltUrl (_urlParts.pathname) + '.jst'};
+			},
+			builder:function (_inputs) {
+				return this.readFile ({path:_inputs.jstTemplate}) ();
+			}
+		};
+	}
+});
+
