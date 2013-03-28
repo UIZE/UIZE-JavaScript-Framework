84ce4b4681ff1427601f78d3a9b5dbffee723559
Chris van Rensburg <uize-git@tomkidding.com>
2013-03-28 11:05:20 -0700

    Added some test files to establish that subclassing of widgets deals correctly with styling overrides.

--- site-source/js/Uize.WidgetV2.AnotherTestWidget.Subclass.js
+++ site-source/js/Uize.WidgetV2.AnotherTestWidget.Subclass.js
@@ -1,7 +1,7 @@
 /*______________
 |       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
 |     /      /  |   ---------------------------------------------------
-|    /    O /   |    MODULE : Uize.WidgetV2.AnotherTestWidget Class
+|    /    O /   |    MODULE : Uize.WidgetV2.AnotherTestWidget.Subclass Class
 |   /    / /    |
 |  /    / /  /| |    ONLINE : http://www.uize.com
 | /____/ /__/_| | COPYRIGHT : (c)2013 UIZE
@@ -18,27 +18,22 @@
 
 /*?
 	Introduction
-		The =Uize.WidgetV2.AnotherTestWidget= class implements a test widget being used during development of the new =Uize.WidgetV2= widget base class to test this class' functionality.
+		The =Uize.WidgetV2.AnotherTestWidget.Subclass= class implements a test widget being used during development of the new =Uize.WidgetV2= widget base class to test this class' functionality.
 
 		*DEVELOPERS:* `Chris van Rensburg`
 */
 
 Uize.module ({
-	name:'Uize.WidgetV2.AnotherTestWidget',
+	name:'Uize.WidgetV2.AnotherTestWidget.Subclass',
 	required:[
-		'Uize.Templates.WidgetV2AnotherTestWidget',
-		'Uize.WidgetV2.AnotherTestWidgetCss'
+		'Uize.WidgetV2.AnotherTestWidget.SubclassCss'
 	],
 	builder:function (_superclass) {
 		'use strict';
 
 		var _class = _superclass.subclass ({
-			set:{
-				html:Uize.Templates.WidgetV2AnotherTestWidget
-			},
-
 			staticProperties:{
-				cssModule:Uize.WidgetV2.AnotherTestWidgetCss
+				cssModule:Uize.WidgetV2.AnotherTestWidget.SubclassCss
 			}
 		});
 
