63576bfe7d07cfe406f6c792b3c07f6b0c16af53
Chris van Rensburg <uize-git@tomkidding.com>
2013-05-01 14:43:43 -0700

    Added a test to ensure that the variadic form of the @required template directive populates the required list correctly.

--- site-source/js/Uize.Test.Uize.Template.js
+++ site-source/js/Uize.Test.Uize.Template.js
@@ -40,6 +40,15 @@ Uize.module ({
 					['Uize.Template.defineStandardEncoding',[
 					]],
 					['Uize.Template.compile',[
+						{
+							title:'Test that, when the variadic form of the @required directive is used, the required list is populated correctly',
+							test:function () {
+								return this.expect (
+									['foo','bar'],
+									Uize.Template.compile ('<%@ required ("foo","bar") %>',{result:'full'}).required
+								);
+							}
+						}
 					]]
 				])
 			]
