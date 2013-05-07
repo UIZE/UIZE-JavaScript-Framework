a84683f33e938d197c7752e9d58ddb08307fe882
Chris van Rensburg <uize-git@tomkidding.com>
2013-05-06 18:56:51 -0700

    Turned the color info widget into a V2 widget and migrated it to under the Uize.Widgets namespace.

---
+++ site-source/js/Uize_Widgets/ColorInfo/VisualTests.js
@@ -0,0 +1,46 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.ColorInfo.VisualTests Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.ColorInfo.VisualTests= class implements a set of visual tests for the =Uize.Widgets.ColorInfo.Widget= class.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.ColorInfo.VisualTests',
+	superclass:'Uize.Widgets.VisualTests.Widget',
+	required:'Uize.Widgets.ColorInfo.Widget',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			omegastructor:function () {
+				this.addStateCombinationTestCases ({
+					value:['#e99975','#4c7cd1','#ff0000','#000','#fff','#888']
+				});
+			},
+
+			staticProperties:{
+				widgetClass:Uize.Widgets.ColorInfo.Widget
+			}
+		});
+	}
+});
+
---
+++ site-source/js/Uize_Widgets/ColorInfo/Widget.js
@@ -0,0 +1,99 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.ColorInfo.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2010-2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 3
+	codeCompleteness: 100
+	docCompleteness: 30
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.ColorInfo.Widget= class implements a basic widget for previewing a color value...
+
+		*DEVELOPERS:* `Chris van Rensburg`
+
+		### In a Nutshell
+			- link to examples
+				- Color Sort by RGB Proximity
+				- Color Gradient Tool
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.ColorInfo.Widget',
+	superclass:'Uize.Widget.V2',
+	required:[
+		'Uize.Color',
+		'Uize.Widgets.ColorInfo.Html',
+		'Uize.Widgets.ColorInfo.Css'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		function _updateUiValue () {
+			var _this = this;
+			if (_this.isWired) {
+				var _colorAsHexRgb = Uize.Color.to (_this._value,'#hex');
+				_this.setNodeValue ('value',_colorAsHexRgb);
+				_this.setNodeStyle (['swatch','asBackground'],{backgroundColor:_colorAsHexRgb});
+				_this.setNodeStyle ('asForeground',{color:_colorAsHexRgb});
+				/*?
+					Implied Nodes
+						value Implied Node
+							.
+
+						swatch
+							.
+
+						asBackground
+							.
+
+						asForeground
+							.
+				*/
+			}
+		}
+
+		return _superclass.subclass ({
+			instanceMethods:{
+				updateUi:_updateUiValue
+			},
+
+			stateProperties:{
+				_value:{
+					name:'value',
+					onChange:_updateUiValue,
+					value:'#000000'
+					/*?
+						State Properties
+							value
+								A value of any type and format supported by the =Uize.Color= module, specifying the current color for which the widget should display information.
+
+								Basically, color values can be specified for this property in any way that a color can be specified when using the single parameter variations of the =Uize.Color= constructor.
+
+								NOTES
+								- the initial value is ='#000000'=
+					*/
+				}
+			},
+
+			set:{
+				html:Uize.Widgets.ColorInfo.Html
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.ColorInfo.Css
+			}
+		});
+	}
+});
+
