9caddedd2ba2cc0647cffee3948b247896b30e11
Chris van Rensburg <uize-git@tomkidding.com>
2013-05-13 20:58:49 -0700

    Created an on/off toggler button widget.

---
+++ site-source/js/Uize_Widgets/Button/Toggle/OnOff/VisualTests.js
@@ -0,0 +1,48 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Button.Toggle.OnOff.VisualTests Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Button.Toggle.OnOff.VisualTests= class implements a set of visual tests for the =Uize.Widgets.Button.Toggle.OnOff.Widget= class.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Button.Toggle.OnOff.VisualTests',
+	superclass:'Uize.Widgets.VisualTests.Widget',
+	required:'Uize.Widgets.Button.Toggle.OnOff.Widget',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			omegastructor:function () {
+				this.addStateCombinationTestCases ({
+					enabled:[true,false],
+					selected:[false,true],
+					size:['tiny','small','medium','large']
+				});
+			},
+
+			staticProperties:{
+				widgetClass:Uize.Widgets.Button.Toggle.OnOff.Widget
+			}
+		});
+	}
+});
+
---
+++ site-source/js/Uize_Widgets/Button/Toggle/OnOff/Widget.js
@@ -0,0 +1,56 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Button.Toggle.OnOff.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2004-2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 8
+	codeCompleteness: 100
+	docCompleteness: 80
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Button.Toggle.OnOff.Widget= class implements a simple on/off toggler button widget class.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Button.Toggle.OnOff.Widget',
+	superclass:'Uize.Widgets.Button.Widget',
+	required:[
+		'Uize.Widgets.Button.Toggle.OnOff.Html',
+		'Uize.Widgets.Button.Toggle.OnOff.Css'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		var _class = _superclass.subclass ({
+			alphastructor:function () {
+				var _this = this;
+				_this.onChange ('selected',function (_selected) {_this.set ({text:_selected ? 'On' : 'Off'})});
+			},
+
+			set:{
+				html:Uize.Widgets.Button.Toggle.OnOff.Html,
+				clickToSelect:true,
+				clickToDeselect:true
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.Button.Toggle.OnOff.Css
+			}
+		});
+
+		return _class;
+	}
+});
+
