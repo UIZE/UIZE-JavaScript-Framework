4fe10cf1b5fa4e47791de577f2af5440ac8ec042
Chris van Rensburg <uize-git@tomkidding.com>
2013-06-15 16:45:33 -0700

    Initial draft of a V2 calculator widget, with HTML, CSS, and support for four UI sizes.

---
+++ site-source/js/Uize_Widgets/Calculator/VisualSampler.js
@@ -0,0 +1,46 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Calculator.VisualSampler Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 5
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Calculator.VisualSampler= class implements a visual sampler widget for the =Uize.Widgets.Calculator.Widget= class.
+
+		*DEVELOPERS:* 
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Calculator.VisualSampler',
+	superclass:'Uize.Widgets.VisualSampler.Widget',
+	required:'Uize.Widgets.Calculator.Widget',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			omegastructor:function () {
+				this.addStateCombinationSamples ({
+					size:['tiny','small','medium']
+				});
+			},
+
+			staticProperties:{
+				widgetClass:Uize.Widgets.Calculator.Widget
+			}
+		});
+	}
+});
+
---
+++ site-source/js/Uize_Widgets/Calculator/Widget.js
@@ -0,0 +1,77 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Calculator.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 5
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Calculator.Widget= module implements a widget class.
+
+		*DEVELOPERS:*
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Calculator.Widget',
+	superclass:'Uize.Widget.Calculator',
+	required:[
+		'Uize.Widget.Button',
+		'Uize.Widgets.Buttons.Char.Widget',
+		'Uize.Widgets.Calculator.Html',
+		'Uize.Widgets.Calculator.Css'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			instanceMethods:{
+				addChild:function (_childName,_childInstanceOrClass,_properties) {
+					return _superclass.doMy (
+						this,
+						'addChild',
+						[
+							_childName,
+							_childInstanceOrClass == Uize.Widget.Button
+								? Uize.Widgets.Buttons.Char.Widget
+								: _childInstanceOrClass,
+							_properties
+						]
+					);
+				}
+			},
+
+			set:{
+				html:Uize.Widgets.Calculator.Html
+			},
+
+			stateProperties:{
+				_size:{
+					name:'size',
+					value:'medium'
+				}
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.Calculator.Css,
+				enableRootNodeCssClasses:false
+			},
+
+			stateToCssBindings:{
+				size:'value'
+			}
+		});
+	}
+});
+
