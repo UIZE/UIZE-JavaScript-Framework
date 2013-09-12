c37c3fd27d2402168adeca5110eb4bae2be09391
Chris van Rensburg <git-uize@tomkidding.com>
2013-09-11 18:40:47 -0700

    Updated this module to use newer coding patterns.

--- site-source/js/Uize.Widget.SegmentDisplay.js
+++ site-source/js/Uize.Widget.SegmentDisplay.js
@@ -29,155 +29,154 @@ Uize.module ({
 	builder:function (_superclass) {
 		'use strict';
 
-		var
-			_class = _superclass.subclass (),
-			_classPrototype = _class.prototype
-		;
-
 		/*** Variables for Performance Optimization ***/
 			var _Uize_Node_Classes_setState = Uize.Node.Classes.setState;
 
 		/*** Private Instance Methods ***/
-			_classPrototype._updateUiSegmentsState = function () {
-				var _this = this;
-				if (_this.isWired) {
+			function _updateUiSegmentsState () {
+				var m = this;
+				if (m.isWired) {
 					var
-						_lastSegmentsState = _this._lastSegmentsState,
-						_segmentsState = _this._segmentsState
+						_lastSegmentsState = m._lastSegmentsState,
+						_segmentsState = m._segmentsState
 					;
-					_this.forEachSegment (
+					m.forEachSegment (
 						function (_segmentNo,_segmentCode,_segmentMask) {
 							var _segmentNewState = _segmentsState & _segmentMask;
 							if (_lastSegmentsState == null || _segmentNewState != (_lastSegmentsState & _segmentMask))
 								_Uize_Node_Classes_setState (
-									_this.getNode ('segment' + _segmentCode),
-									[_this._cssClassSegmentOff,_this._cssClassSegmentOn],
+									m.getNode ('segment' + _segmentCode),
+									[m._cssClassSegmentOff,m._cssClassSegmentOn],
 									!!_segmentNewState
 								)
 							;
 						}
 					);
-					_this._lastSegmentsState = _segmentsState;
+					m._lastSegmentsState = _segmentsState;
 				}
-			};
-
-		/*** Public Instance Methods ***/
-			_classPrototype.forEachSegment = function (_function) {
-				for (
-					var
-						_segmentNo = -1,
-						_segmentCodes = this.Class.segmentCodes,
-						_segmentCodesLength = _segmentCodes.length,
-						_segmentMask = 1 << _segmentCodesLength
-					;
-					++_segmentNo < _segmentCodesLength;
-				)
-					_function.call (this,_segmentNo,_segmentCodes [_segmentNo],_segmentMask = _segmentMask >> 1)
-				;
-			};
-
-			_classPrototype.getSegmentState = function (_segmentNo) {
-				return !!(this._segmentsState & (this.Class._firstSegmentMask >> _segmentNo));
-			};
-
-			_classPrototype.getSegmentsStateAsHex = function (_reverse) {
-				return this.Class.getSegmentsStateAsHex (this._segmentsState,_reverse);
-			};
-
-			_classPrototype.invertAllSegmentsState = function () {
-				this.set ({_segmentsState:this._segmentsState ^ this.Class._allSegmentsMask});
-			};
-
-			_classPrototype.setSegmentState = function (_segmentNo,_state) {
-				this.getSegmentState (_segmentNo) != _state && this.toggleSegmentState (_segmentNo);
-			};
-
-			_classPrototype.setAllSegmentsState = function (_state) {
-				this.set ({_segmentsState:_state ? this.Class._allSegmentsMask : 0});
-			};
-
-			_classPrototype.toggleSegmentState = function (_segmentNo) {
-				this.set ({_segmentsState:this._segmentsState ^ (this.Class._firstSegmentMask >> _segmentNo)});
-			};
-
-			_classPrototype.updateUi = function () {
-				this._updateUiSegmentsState ();
-			};
-
-			_classPrototype.wireUi = function () {
-				var _this = this;
-				if (!_this.isWired) {
-					_this._lastSegmentsState = null;
-
-					_superclass.doMy (_this,'wireUi');
-				}
-			};
+			}
 
 		/*** Private Static Methods ***/
-			_class._conformSegmentsState = function (_segmentsState) {
-				return Math.max (Math.round (_segmentsState),0) & this._allSegmentsMask;
-			};
+			function _conformSegmentsState (_Class,_segmentsState) {
+				return Math.max (Math.round (_segmentsState),0) & _Class._allSegmentsMask;
+			}
 
-		/*** Public Static Methods ***/
-			_class.configureDisplay = function (_segmentCodes,_valueToSegmentsStateLookup) {
-				var segmentCodesLength = (this.segmentCodes = _segmentCodes).length;
-				this.segmentsStateToValueLookup =
-					Uize.reverseLookup (this.valueToSegmentsStateLookup = _valueToSegmentsStateLookup)
-				;
-				this._allSegmentsMask = (1 << segmentCodesLength) - 1;
-				this._firstSegmentMask = 1 << (segmentCodesLength - 1);
-				this._toHexExtraByteBit = 1 << (Math.ceil (segmentCodesLength / 8) * 8);
-				/*?
-					###
-						Static Properties
-							segmentCodes
-								.
-
-							segmentsStateToValueLookup
-								.
-
-							valueToSegmentsStateLookup
-								.
-				*/
-			};
-
-			_class.getSegmentsStateAsHex = function (_segmentsState,_reverse) {
-				_segmentsState = this._conformSegmentsState (_segmentsState);
-				if (_reverse) {
-					for (var _leftBitMask = this._firstSegmentMask, _rightBitMask = 1; _leftBitMask > _rightBitMask;) {
-						if (!!(_segmentsState & _leftBitMask) ^ !!(_segmentsState & _rightBitMask))
-							_segmentsState = _segmentsState ^ _leftBitMask ^ _rightBitMask
+		return _superclass.subclass ({
+			instanceMethods:{
+				forEachSegment:function (_function) {
+					for (
+						var
+							m = this,
+							_segmentNo = -1,
+							_segmentCodes = m.Class.segmentCodes,
+							_segmentCodesLength = _segmentCodes.length,
+							_segmentMask = 1 << _segmentCodesLength
 						;
-						_leftBitMask = _leftBitMask >> 1;
-						_rightBitMask = _rightBitMask << 1;
+						++_segmentNo < _segmentCodesLength;
+					)
+						_function.call (m,_segmentNo,_segmentCodes [_segmentNo],_segmentMask = _segmentMask >> 1)
+					;
+				},
+
+				getSegmentState:function (_segmentNo) {
+					return !!(this._segmentsState & (this.Class._firstSegmentMask >> _segmentNo));
+				},
+
+				getSegmentsStateAsHex:function (_reverse) {
+					return this.Class.getSegmentsStateAsHex (this._segmentsState,_reverse);
+				},
+
+				invertAllSegmentsState:function () {
+					this.set ({_segmentsState:this._segmentsState ^ this.Class._allSegmentsMask});
+				},
+
+				setSegmentState:function (_segmentNo,_state) {
+					this.getSegmentState (_segmentNo) != _state && this.toggleSegmentState (_segmentNo);
+				},
+
+				setAllSegmentsState:function (_state) {
+					this.set ({_segmentsState:_state ? this.Class._allSegmentsMask : 0});
+				},
+
+				toggleSegmentState:function (_segmentNo) {
+					this.set ({_segmentsState:this._segmentsState ^ (this.Class._firstSegmentMask >> _segmentNo)});
+				},
+
+				updateUi:_updateUiSegmentsState,
+
+				wireUi:function () {
+					var m = this;
+					if (!m.isWired) {
+						m._lastSegmentsState = null;
+
+						_superclass.doMy (m,'wireUi');
 					}
 				}
-				return '0x' + (_segmentsState | this._toHexExtraByteBit).toString (16).slice (1);
-			};
+			},
 
-			_class.getSegmentsStateFromValue = function (_value) {
-				if ((_value += '').length == 1) {
+			staticMethods:{
+				configureDisplay:function (_segmentCodes,_valueToSegmentsStateLookup) {
 					var
-						_valueToSegmentsStateLookup = this.valueToSegmentsStateLookup,
-						_segmentsState = _valueToSegmentsStateLookup [_value]
+						_Class = this,
+						_segmentCodesLength = (_Class.segmentCodes = _segmentCodes).length
 					;
-					if (_segmentsState == null) _segmentsState = _valueToSegmentsStateLookup [_value.toLowerCase ()];
-					if (_segmentsState == null) _segmentsState = _valueToSegmentsStateLookup [_value.toUpperCase ()];
-					return _segmentsState || 0;
-				} else {
-					return this._conformSegmentsState (_value);
+					_Class.segmentsStateToValueLookup =
+						Uize.reverseLookup (_Class.valueToSegmentsStateLookup = _valueToSegmentsStateLookup)
+					;
+					_Class._allSegmentsMask = (1 << _segmentCodesLength) - 1;
+					_Class._firstSegmentMask = 1 << (_segmentCodesLength - 1);
+					_Class._toHexExtraByteBit = 1 << (Math.ceil (_segmentCodesLength / 8) * 8);
+					/*?
+						###
+							Static Properties
+								segmentCodes
+									.
+
+								segmentsStateToValueLookup
+									.
+
+								valueToSegmentsStateLookup
+									.
+					*/
+				},
+
+				getSegmentsStateAsHex:function (_segmentsState,_reverse) {
+					_segmentsState = _conformSegmentsState (this,_segmentsState);
+					if (_reverse) {
+						for (var _leftBitMask = this._firstSegmentMask, _rightBitMask = 1; _leftBitMask > _rightBitMask;) {
+							if (!!(_segmentsState & _leftBitMask) ^ !!(_segmentsState & _rightBitMask))
+								_segmentsState = _segmentsState ^ _leftBitMask ^ _rightBitMask
+							;
+							_leftBitMask = _leftBitMask >> 1;
+							_rightBitMask = _rightBitMask << 1;
+						}
+					}
+					return '0x' + (_segmentsState | this._toHexExtraByteBit).toString (16).slice (1);
+				},
+
+				getSegmentsStateFromValue:function (_value) {
+					if ((_value += '').length == 1) {
+						var
+							_valueToSegmentsStateLookup = this.valueToSegmentsStateLookup,
+							_segmentsState = _valueToSegmentsStateLookup [_value]
+						;
+						if (_segmentsState == null) _segmentsState = _valueToSegmentsStateLookup [_value.toLowerCase ()];
+						if (_segmentsState == null) _segmentsState = _valueToSegmentsStateLookup [_value.toUpperCase ()];
+						return _segmentsState || 0;
+					} else {
+						return _conformSegmentsState (this,_value);
+					}
+				},
+
+				getValueFromSegmentsState:function (_segmentsState) {
+					return (
+						this.segmentsStateToValueLookup [_segmentsState = _conformSegmentsState (this,_segmentsState)] ||
+						this.getSegmentsStateAsHex (_segmentsState)
+					);
 				}
-			};
+			},
 
-			_class.getValueFromSegmentsState = function (_segmentsState) {
-				return (
-					this.segmentsStateToValueLookup [_segmentsState = this._conformSegmentsState (_segmentsState)] ||
-					this.getSegmentsStateAsHex (_segmentsState)
-				);
-			};
-
-		/*** State Properties ***/
-			_class.stateProperties ({
+			stateProperties:{
 				_cssClassSegmentOff:{
 					name:'cssClassSegmentOff',
 					value:'sevenSegSegmentOff'
@@ -188,10 +187,10 @@ Uize.module ({
 				},
 				_segmentsState:{
 					name:'segmentsState',
-					conformer:function (_value) {return this.Class._conformSegmentsState (_value)},
+					conformer:function (_value) {return _conformSegmentsState (this.Class,_value)},
 					onChange:[
 						function () {this.set ({_value:this.Class.getValueFromSegmentsState (this._segmentsState)})},
-						_classPrototype._updateUiSegmentsState
+						_updateUiSegmentsState
 					]
 				},
 				_value:{
@@ -201,9 +200,8 @@ Uize.module ({
 					},
 					onChange:function () {this.set ({_segmentsState:this.Class.getSegmentsStateFromValue (this._value)})}
 				}
-			});
-
-		return _class;
+			}
+		});
 	}
 });
 
