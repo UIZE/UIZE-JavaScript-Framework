1b0750bd3ab2ce1ede228ff900991f716e52609a
Chris van Rensburg <git-uize@tomkidding.com>
2013-09-19 19:24:19 -0700

    Progress with module dependency analysis.

---
+++ site-source/js/Uize_Widgets/SimpleStatsTable/VisualSampler.js
@@ -0,0 +1,75 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.SimpleStatsTable.VisualSampler Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 5
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.SimpleStatsTable.VisualSampler= class implements a visual sampler widget for the =Uize.Widgets.SimpleStatsTable.Widget= class.
+
+		*DEVELOPERS:*
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.SimpleStatsTable.VisualSampler',
+	superclass:'Uize.Widgets.VisualSampler.Widget',
+	required:'Uize.Widgets.SimpleStatsTable.Widget',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			omegastructor:function () {
+				this.addSample ({
+					title:'This is the Table Heading',
+					columns:[
+						{
+							title:'Item Type Heading'
+						},
+						{
+							title:'Column 1 Heading',
+							minColor:'hsl(200,100,0)',
+							maxColor:'hsl(200,100,75)'
+						},
+						{
+							title:'Column 2 Heading',
+							minColor:'hsl(120,100,50)',
+							maxColor:'hsl(0,100,50)'
+						},
+						{
+							title:'Column 3 Heading',
+							minColor:'hsl(120,100,50)',
+							maxColor:'hsl(0,100,50)',
+							minValue:0,
+							maxValue:100
+						}
+					],
+					rows:[
+						['Row 1',0,12,100],
+						['Row 2',15,-5,32],
+						['Row 3',27,5,70],
+						['Row 4',3,19,50],
+						['Row 5',20,1,79]
+					]
+				});
+			},
+
+			staticProperties:{
+				widgetClass:Uize.Widgets.SimpleStatsTable.Widget
+			}
+		});
+	}
+});
+
---
+++ site-source/js/Uize_Widgets/SimpleStatsTable/Widget.js
@@ -0,0 +1,64 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.SimpleStatsTable.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 5
+	docCompleteness: 5
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.SimpleStatsTable.Widget= module implements a widget class.
+
+		*DEVELOPERS:*
+
+		Visual Sampler
+			Below is a visual sampler of the =Uize.Widgets.SimpleStatsTable.Widget= class...
+
+			........................................................
+			<< widget >>
+
+			widgetClass: Uize.Widgets.SimpleStatsTable.VisualSampler
+			........................................................
+*/
+
+/* TODO:
+	- implement sorting
+	- support configurable tooltip handler for item name column
+	- support configurable click action for item name column
+	- support configurable value formatting per column
+	- support any column not being a value type column (would handle case of module type column in SOTU table)
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.SimpleStatsTable.Widget',
+	superclass:'Uize.Widget.V2',
+	required:[
+		'Uize.Widgets.SimpleStatsTable.Html',
+		'Uize.Widgets.SimpleStatsTable.Css'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			set:{
+				html:Uize.Widgets.SimpleStatsTable.Html
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.SimpleStatsTable.Css
+			}
+		});
+	}
+});
+
