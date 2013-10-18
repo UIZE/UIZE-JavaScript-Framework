1f1aa2e129a4b9a69bee60c8f7317bf78896ff44
Chris van Rensburg <git-uize@tomkidding.com>
2013-10-18 10:49:49 -0700

    Uize.String.Builder gets migrated to Uize.Str.Builder.

--- site-source/js/Uize_Test/Uize/Str/Builder.js
+++ site-source/js/Uize_Test/Uize/Str/Builder.js
@@ -1,7 +1,7 @@
 /*______________
 |       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
 |     /      /  |   ---------------------------------------------------
-|    /    O /   |    MODULE : Uize.Test.Uize.String.Builder Class
+|    /    O /   |    MODULE : Uize.Test.Uize.Str.Builder Class
 |   /    / /    |
 |  /    / /  /| |    ONLINE : http://www.uize.com
 | /____/ /__/_| | COPYRIGHT : (c)2010-2013 UIZE
@@ -18,19 +18,19 @@
 
 /*?
 	Introduction
-		The =Uize.Test.Uize.String.Builder= module defines a suite of unit tests for the =Uize.String.Builder= module.
+		The =Uize.Test.Uize.Str.Builder= module defines a suite of unit tests for the =Uize.Str.Builder= module.
 
 		*DEVELOPERS:* `Chris van Rensburg`
 */
 
 Uize.module ({
-	name:'Uize.Test.Uize.String.Builder',
+	name:'Uize.Test.Uize.Str.Builder',
 	required:'Uize.Data',
 	builder:function () {
 		'use strict';
 
 		function _getHelloStringBuilderAppended () {
-			var _stringBuilder = Uize.String.Builder ();
+			var _stringBuilder = Uize.Str.Builder ();
 			_stringBuilder.append ('H');
 			_stringBuilder.append ('e');
 			_stringBuilder.append ('l');
@@ -41,49 +41,49 @@ Uize.module ({
 		}
 
 		return Uize.Test.resolve ({
-			title:'Test for Uize.String.Builder Module',
+			title:'Test for Uize.Str.Builder Module',
 			test:[
-				Uize.Test.requiredModulesTest ('Uize.String.Builder'),
+				Uize.Test.requiredModulesTest ('Uize.Str.Builder'),
 				{
 					title:'Test creating an instance with no constructor arguments (should initialize to empty string)',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ();
+						var _stringBuilder = Uize.Str.Builder ();
 						return _stringBuilder.length === 0 && _stringBuilder.valueOf () === '';
 					}
 				},
 				{
 					title:'Test creating an instance with a non-empty string as initial value',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ('hello');
+						var _stringBuilder = Uize.Str.Builder ('hello');
 						return _stringBuilder.length === 5 && _stringBuilder.valueOf () == 'hello';
 					}
 				},
 				{
 					title:'Test creating an instance with a number as initial value',
-					test:function () {return (Uize.String.Builder (1234567)).valueOf () === '1234567'}
+					test:function () {return (Uize.Str.Builder (1234567)).valueOf () === '1234567'}
 				},
 				{
 					title:'Test creating an instance with a boolean as initial value',
-					test:function () {return (Uize.String.Builder (true)).valueOf () === 'true'}
+					test:function () {return (Uize.Str.Builder (true)).valueOf () === 'true'}
 				},
 				{
 					title:'Test that equals instance method works when strings are equal',
-					test:function () {return (Uize.String.Builder ('hello')).equals ('hello')}
+					test:function () {return (Uize.Str.Builder ('hello')).equals ('hello')}
 				},
 				{
 					title:'Test that equals instance method works with no parameters (ie. testing for empty string)',
 					test:function () {
-						return Uize.String.Builder ().equals () && !Uize.String.Builder ('hello').equals ();
+						return Uize.Str.Builder ().equals () && !Uize.Str.Builder ('hello').equals ();
 					}
 				},
 				{
 					title:'Test that equals instance method works when strings are not equal',
-					test:function () {return !Uize.String.Builder ('hello').equals ('goodbye')}
+					test:function () {return !Uize.Str.Builder ('hello').equals ('goodbye')}
 				},
 				{
 					title:'Test that getValue, valueOf, and toString instance methods all return the same result',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ('hello');
+						var _stringBuilder = Uize.Str.Builder ('hello');
 						return (
 							_stringBuilder.getValue () == 'hello' &&
 							_stringBuilder.valueOf () == 'hello' &&
@@ -93,12 +93,12 @@ Uize.module ({
 				},
 				{
 					title:'Test that coercion of instance to string works correctly',
-					test:function () {return Uize.String.Builder ('hello') + '' == 'hello'}
+					test:function () {return Uize.Str.Builder ('hello') + '' == 'hello'}
 				},
 				{
 					title:'Test that append instance method works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ();
+						var _stringBuilder = Uize.Str.Builder ();
 						_stringBuilder.append ('h');
 						_stringBuilder.append ('e');
 						_stringBuilder.append ('l');
@@ -110,7 +110,7 @@ Uize.module ({
 				{
 					title:'Test that prepend instance method works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ();
+						var _stringBuilder = Uize.Str.Builder ();
 						_stringBuilder.prepend ('o');
 						_stringBuilder.prepend ('l');
 						_stringBuilder.prepend ('l');
@@ -122,7 +122,7 @@ Uize.module ({
 				{
 					title:'Test that append and prepend instance methods work correctly in combination',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ();
+						var _stringBuilder = Uize.Str.Builder ();
 						_stringBuilder.append ('l');
 						_stringBuilder.prepend ('e');
 						_stringBuilder.append ('l');
@@ -134,7 +134,7 @@ Uize.module ({
 				{
 					title:'Test that appending a number value works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder (123);
+						var _stringBuilder = Uize.Str.Builder (123);
 						_stringBuilder.append (4567);
 						return _stringBuilder.length === 7 && _stringBuilder.valueOf () == '1234567';
 					}
@@ -142,7 +142,7 @@ Uize.module ({
 				{
 					title:'Test that appending a boolean value works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder (false);
+						var _stringBuilder = Uize.Str.Builder (false);
 						_stringBuilder.append (true);
 						return _stringBuilder.length === 9 && _stringBuilder.valueOf () == 'falsetrue';
 					}
@@ -150,7 +150,7 @@ Uize.module ({
 				{
 					title:'Test that prepending a number value works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder (4567);
+						var _stringBuilder = Uize.Str.Builder (4567);
 						_stringBuilder.prepend (123);
 						return _stringBuilder.length === 7 && _stringBuilder.valueOf () == '1234567';
 					}
@@ -158,7 +158,7 @@ Uize.module ({
 				{
 					title:'Test that prepending a boolean value works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder (true);
+						var _stringBuilder = Uize.Str.Builder (true);
 						_stringBuilder.prepend (false);
 						return _stringBuilder.length === 9 && _stringBuilder.valueOf () == 'falsetrue';
 					}
@@ -166,7 +166,7 @@ Uize.module ({
 				{
 					title:'Test that clear instance method works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ('hello');
+						var _stringBuilder = Uize.Str.Builder ('hello');
 						_stringBuilder.clear ();
 						return _stringBuilder.length === 0 && _stringBuilder.valueOf () === '';
 					}
@@ -174,7 +174,7 @@ Uize.module ({
 				{
 					title:'Test that setValue instance method with non-empty string works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ('hello');
+						var _stringBuilder = Uize.Str.Builder ('hello');
 						_stringBuilder.setValue ('goodbye');
 						return _stringBuilder.length === 7 && _stringBuilder.valueOf () == 'goodbye';
 					}
@@ -182,7 +182,7 @@ Uize.module ({
 				{
 					title:'Test that setValue instance method with no parameters works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ('hello');
+						var _stringBuilder = Uize.Str.Builder ('hello');
 						_stringBuilder.setValue ();
 						return _stringBuilder.length === 0 && _stringBuilder.valueOf () === '';
 					}
@@ -190,7 +190,7 @@ Uize.module ({
 				{
 					title:'Test that setValue instance method with number value works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ('hello');
+						var _stringBuilder = Uize.Str.Builder ('hello');
 						_stringBuilder.setValue (1234567);
 						return _stringBuilder.length === 7 && _stringBuilder.valueOf () === '1234567';
 					}
@@ -198,7 +198,7 @@ Uize.module ({
 				{
 					title:'Test that setValue instance method with boolean value works correctly',
 					test:function () {
-						var _stringBuilder = Uize.String.Builder ('hello');
+						var _stringBuilder = Uize.Str.Builder ('hello');
 						_stringBuilder.setValue (true);
 						return _stringBuilder.length === 4 && _stringBuilder.valueOf () === 'true';
 					}
