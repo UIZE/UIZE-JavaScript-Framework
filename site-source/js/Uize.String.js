0171382af98d0a929e6ce88d6bcbccc4b0237477
Chris van Rensburg <git-uize@tomkidding.com>
2013-10-21 22:35:35 -0700

    Added some docs to the Uize.String module detailing the deprecation of this module.

--- site-source/js/Uize.String.js
+++ site-source/js/Uize.String.js
@@ -22,8 +22,35 @@
 
 		*DEVELOPERS:* `Chris van Rensburg`
 
-		The deprecated =Uize.String= module aggregates methods from various modules in order to provide backwards compatibility for the static methods that have been migrated into separate modules under the =Uize.Str= and =Uize.Array= namespaces. For full details, consult the [[../news/2013-??-??-Uize.String-module-deprecated.html][news announcement]] for this change.
+		Deprecated
+			The =Uize.String= module has been deprecated in favor of various, smaller modules under the =Uize.Str= and =Uize.Array= namespaces.
 
+			In order to maintain backwards compatibility while this module is still in the deprecated state, and before it is killed entirely, this module aggregates methods from the various newer modules contained under the =Uize.Str= and =Uize.Array= namespaces. All the static methods that have been migrated to other modules are still supported in this module and behave in exactly the same way as before.
+
+			In order to upgrade your code to prepare for the eventual expiration of this backwards compatibility provision, you should change all calls to the methods of =Uize.String= module in your code, according to the following map...
+
+			..........................................................................................
+			Uize.String.hugJoin                  >> BECOMES >>  Uize.Array.Join.hugJoin
+
+			Uize.String.limitLength              >> BECOMES >>  Uize.Str.Limit.limitLength
+			Uize.String.joinUsingSuffixPriority  >> BECOMES >>  Uize.Str.Limit.joinUsingSuffixPriority
+
+			Uize.String.contains                 >> BECOMES >>  Uize.Str.Has.has
+			Uize.String.startsWith               >> BECOMES >>  Uize.Str.Has.hasPrefix
+			Uize.String.endsWith                 >> BECOMES >>  Uize.Str.Has.hasSuffix
+
+			Uize.String.toCamel                  >> BECOMES >>  Uize.Str.Camel.to
+
+			Uize.String.repeat                   >> BECOMES >>  Uize.Str.Repeat.repeat
+
+			Uize.String.split                    >> BECOMES >>  Uize_Str_Split.split
+			Uize.String.splitInTwo               >> BECOMES >>  Uize_Str_Split.splitInTwo
+
+			Uize.String.hasPadding               >> BECOMES >>  Uize.Str.Trim.hasPadding
+			Uize.String.trim                     >> BECOMES >>  Uize.Str.Trim.trim
+			Uize.String.trimLeft                 >> BECOMES >>  Uize.Str.Trim.trimLeft
+			Uize.String.trimRight                >> BECOMES >>  Uize.Str.Trim.trimRight
+			..........................................................................................
 */
 
 Uize.module ({
@@ -52,31 +79,108 @@ Uize.module ({
 		return Uize.package ({
 			/*** deprecated methods migrated to the Uize.Array.Join module ***/
 				hugJoin:Uize.Array.Join.hugJoin,
+					/*?
+						Static Methods
+							Uize.String.hugJoin
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Array.Join.hugJoin= method implemented in the =Uize.Array.Join= module.
+					*/
 
 			/*** deprecated methods migrated to the Uize.Str.Has module ***/
 				limitLength:_Uize_Str_Limit.limitLength,
+					/*?
+						Static Methods
+							Uize.String.limitLength
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Limit.limitLength= method implemented in the =Uize.Str.Limit= module.
+					*/
+
 				joinUsingSuffixPriority:_Uize_Str_Limit.joinUsingSuffixPriority,
+					/*?
+						Static Methods
+							Uize.String.joinUsingSuffixPriority
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Limit.joinUsingSuffixPriority= method implemented in the =Uize.Str.Limit= module.
+					*/
 
 			/*** deprecated methods migrated to the Uize.Str.Has module ***/
 				contains:_Uize_Str_Has.has,
+					/*?
+						Static Methods
+							Uize.String.contains
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Has.has= method implemented in the =Uize.Str.Has= module.
+					*/
+
 				startsWith:_Uize_Str_Has.hasPrefix,
+					/*?
+						Static Methods
+							Uize.String.startsWith
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Has.hasPrefix= method implemented in the =Uize.Str.Has= module.
+					*/
+
 				endsWith:_Uize_Str_Has.hasSuffix,
+					/*?
+						Static Methods
+							Uize.String.endsWith
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Has.hasSuffix= method implemented in the =Uize.Str.Has= module.
+					*/
 
 			/*** deprecated methods migrated to the Uize.Str.Camel module ***/
 				toCamel:_Uize_Str.Camel.to,
+					/*?
+						Static Methods
+							Uize.String.toCamel
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Camel.to= method implemented in the =Uize.Str.Camel= module.
+					*/
 
 			/*** deprecated methods migrated to the Uize.Str.Repeat module ***/
 				repeat:_Uize_Str.Repeat.repeat,
+					/*?
+						Static Methods
+							Uize.String.repeat
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Repeat.repeat= method implemented in the =Uize.Str.Repeat= module.
+					*/
 
 			/*** deprecated methods migrated to the Uize.Str.Split module ***/
 				split:_Uize_Str_Split.split,
+					/*?
+						Static Methods
+							Uize.String.split
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Split.split= method implemented in the =Uize.Str.Split= module.
+					*/
+
 				splitInTwo:_Uize_Str_Split.splitInTwo,
+					/*?
+						Static Methods
+							Uize.String.splitInTwo
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Split.splitInTwo= method implemented in the =Uize.Str.Split= module.
+					*/
 
 			/*** deprecated methods migrated to the Uize.Str.Trim module ***/
 				hasPadding:_Uize_Str_Trim.hasPadding,
+					/*?
+						Static Methods
+							Uize.String.hasPadding
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Trim.hasPadding= method implemented in the =Uize.Str.Trim= module.
+					*/
+
 				trim:_Uize_Str_Trim.trim,
+					/*?
+						Static Methods
+							Uize.String.trim
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Trim.trim= method implemented in the =Uize.Str.Trim= module.
+					*/
+
 				trimLeft:_Uize_Str_Trim.trimLeft,
+					/*?
+						Static Methods
+							Uize.String.trimLeft
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Trim.trimLeft= method implemented in the =Uize.Str.Trim= module.
+					*/
+
 				trimRight:_Uize_Str_Trim.trimRight
+					/*?
+						Static Methods
+							Uize.String.trimRight
+								This method has been deprecated *(DEPRECATED 2013-10-21)* in favor of the new =Uize.Str.Trim.trimRight= method implemented in the =Uize.Str.Trim= module.
+					*/
 		});
 	}
 });
