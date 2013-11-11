9a787a9856d5d52f87e42ad35149232a63a7445c
Ben Ilegbodu <ben@zazzle.com>
2013-11-11 09:40:36 -0800

    Somehow duplicated childId.  Not sure how I missed that in the diff

--- site-source/js/Uize.Widget.js
+++ site-source/js/Uize.Widget.js
@@ -1624,31 +1624,6 @@ Uize.module ({
 							*/
 						},
 
-						childId:function (_childName) {
-							return _constructIdPrefix (this._idPrefix, this._idPrefix, _childName, this._idPrefixConstruction);
-							/*?
-								Instance Methods
-									childId
-										Returns the ID prefix of a child widget with the specified =childNameSTR=.
-
-										SYNTAX
-										..................................................
-										childIdPrefixSTR = myWidget.childId(childNameSTR);
-										..................................................
-
-										EXAMPLE
-										...........................................
-										var childIdPrefix = mySlider.childId('child');
-										...........................................
-
-										Assuming an =idPrefix= of ="page_slider"=, the example would return ="page_slider_child"=.
-
-										NOTES
-										- See related =addChild= instance method in =Uize.Widget=
-										- See also =Uize.Widget.constructIdPrefix= static method
-							*/
-						},
-
 					/*** Overridable Wiring and Updating Methods ***/
 						kill:function () {
 							_callOn (this._children,'kill');
