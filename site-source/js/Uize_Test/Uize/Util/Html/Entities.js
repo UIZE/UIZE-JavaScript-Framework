7b26afea714e446e2095d5062e3bcc0237a5c98f
Chris van Rensburg <git-uize@tomkidding.com>
2013-11-23 10:54:04 -0800

    Added unit tests for Uize.Util.Html.Entities module, and finished documentation and added news announcement.

---
+++ site-source/js/Uize_Test/Uize/Util/Html/Entities.js
@@ -0,0 +1,62 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Test.Uize.Util.Html.Entities Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2013 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Test
+	importance: 0
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Test.Uize.Util.Html.Entities= module defines a suite of unit tests for the =Uize.Util.Html.Entities= module.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Test.Uize.Util.Html.Entities',
+	required:'Uize.Class.Value',
+	builder:function () {
+		'use strict';
+
+		return Uize.Test.resolve ({
+			title:'Test for Uize.Util.Html.Entities Module',
+			test:[
+				Uize.Test.requiredModulesTest ('Uize.Util.Html.Entities'),
+				Uize.Test.staticPropertyTest ('Uize.Util.Html.Entities.entityNameToCharCodeLookup','object'),
+				{
+					title:'Test that the Uize.Util.Html.Entities.entityNameToCharCodeLookup static property has properties with entity names as keys and entity character codes as values',
+					test:function () {
+						var _entityNameToCharCodeLookup = Uize.Util.Html.Entities.entityNameToCharCodeLookup;
+						return (
+							this.expect (169,_entityNameToCharCodeLookup.copy) &&
+							this.expect (9824,_entityNameToCharCodeLookup.spades)
+						);
+					}
+				},
+				Uize.Test.staticPropertyTest ('Uize.Util.Html.Entities.charCodeToEntityNameLookup','object'),
+				{
+					title:'Test that the Uize.Util.Html.Entities.charCodeToEntityNameLookup static property has properties with entity character codes as keys and entity names as values',
+					test:function () {
+						var _charCodeToEntityNameLookup = Uize.Util.Html.Entities.charCodeToEntityNameLookup;
+						return (
+							this.expect ('copy',_charCodeToEntityNameLookup [169]) &&
+							this.expect ('spades',_charCodeToEntityNameLookup [9824])
+						);
+					}
+				}
+			]
+		});
+	}
+});
+
