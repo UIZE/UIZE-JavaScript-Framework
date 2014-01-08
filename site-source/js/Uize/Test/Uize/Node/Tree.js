4932d31ff4a0e810f4ea1939903d7ee2394ee5dd
Chris van Rensburg <git-uize@tomkidding.com>
2014-01-08 09:05:04 -0800

    Unit test modules for the Uize.Node.* alias modules.

---
+++ site-source/js/Uize/Test/Uize/Node/Tree.js
@@ -0,0 +1,34 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Test.Uize.Node.Tree Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Test
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Test.Uize.Node.Tree= module defines unit tests to verify that the deprecated =Uize.Node.Tree= module is still supported and is a reference to the newer =Uize.Dom.Tree= module.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Test.Uize.Node.Tree',
+	builder:function () {
+		'use strict';
+
+		return Uize.Test.moduleAliasTest ('Uize.Node.Tree','Uize.Dom.Tree');
+	}
+});
+
