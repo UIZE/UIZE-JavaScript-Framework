798e6e23142ddbe70210205f139d6e4e944cc44d
Chris van Rensburg <git-uize@tomkidding.com>
2014-01-20 20:07:25 -0800

    Chipping away at XML parsing.

--- site-source/js/Uize/Test/Uize/Util/Xml/Tag.js
+++ site-source/js/Uize/Test/Uize/Util/Xml/Tag.js
@@ -1,7 +1,7 @@
 /*______________
 |       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
 |     /      /  |   ---------------------------------------------------
-|    /    O /   |    MODULE : Uize.Test.Uize.Util.Xml.TagName Class
+|    /    O /   |    MODULE : Uize.Test.Uize.Util.Xml.Tag Class
 |   /    / /    |
 |  /    / /  /| |    ONLINE : http://www.uize.com
 | /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
@@ -18,13 +18,13 @@
 
 /*?
 	Introduction
-		The =Uize.Test.Uize.Util.Xml.TagName= module defines a suite of unit tests for the =Uize.Util.Xml.TagName= module.
+		The =Uize.Test.Uize.Util.Xml.Tag= module defines a suite of unit tests for the =Uize.Util.Xml.Tag= module.
 
 		*DEVELOPERS:* `Chris van Rensburg`
 */
 
 Uize.module ({
-	name:'Uize.Test.Uize.Util.Xml.TagName',
+	name:'Uize.Test.Uize.Util.Xml.Tag',
 	builder:function () {
 		'use strict';
 
@@ -33,10 +33,10 @@ Uize.module ({
 			return {
 				title:_title,
 				test:function () {
-					var _parser = new Uize.Util.Xml.TagName;
+					var _parser = new Uize.Util.Xml.Tag;
 					_parser.parse (_sourceStr);
 					return (
-						this.expect (_expectedParsedSegment,_parser.tagName) &&
+						this.expect (_expectedParsedSegment,_parser.tag) &&
 						this.expect (_expectedParsedSegment.length,_parser.length) &&
 						this.expect (!!_expectedParsedSegment,_parser.isValid)
 					);
@@ -45,26 +45,29 @@ Uize.module ({
 		}
 
 		return Uize.Test.resolve ({
-			title:'Test for Uize.Util.Xml.TagName Module',
+			title:'Test for Uize.Util.Xml.Tag Module',
 			test:[
-				Uize.Test.requiredModulesTest ('Uize.Util.Xml.TagName'),
+				Uize.Test.requiredModulesTest ([
+					'Uize.Util.Xml.NodeList',
+					'Uize.Util.Xml.Tag'
+				]),
 				{
 					title:'',
 					test:[
 						_parserTest (
-							'Test that a string fails parsing as a tag name if the first character is not a valid tag name start character',
-							'0img',
+							'',
+							'<',
 							''
 						),
 						_parserTest (
-							'Test that a string is successfully parsed as a tag name if its first character is a valid tag name start character',
-							'_:::',
-							'_'
+							'',
+							'<img',
+							'<img'
 						),
 						_parserTest (
-							'Test that all characters, following the initial start character, that are valid tag name continuation characters are included in the parse tag name',
-							'__abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789:::',
-							'__abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789'
+							'',
+							'<img>',
+							'<img'
 						)
 					]
 				}
