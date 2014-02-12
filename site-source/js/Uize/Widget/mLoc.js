4cba883c5007415da7da00552280a3d4864ef473
Chris van Rensburg <git-uize@tomkidding.com>
2014-02-12 06:22:22 +0200

    The beginnings of a new mLoc mixin for the localization features of next generation widgets.

---
+++ site-source/js/Uize/Widget/mLoc.js
@@ -0,0 +1,102 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widget.mLoc Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 6
+	codeCompleteness: 2
+	docCompleteness: 2
+*/
+
+/*?
+	Introduction
+		The =Uize.Widget.mLoc= mixin module implements features to facilitate the creation of localized widget classes.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Widget.mLoc',
+	builder:function () {
+		'use strict';
+
+		var _localeStringsLookup = {};
+
+		/*** Utility Methods ***/
+			function _needLocaleStrings (_module,_locale,_callback) {
+				if (_module) {
+					var
+						_moduleName = _module.moduleName,
+						_localeStringsId = _moduleName + ':' + _locale
+					;
+					if (_localeStringsLookup [_localeStringsId]) {
+						_callback (_localeStringsLookup [_localeStringsId]);
+					} else {
+						_needLocaleStrings (
+							_module.superclass,
+							_locale,
+							function (_inheritedLocaleStrings) {
+								function _setLocaleStringsAndProvide (_moduleLocaleStrings) {
+									_callback (
+										_localeStringsLookup [_localeStringsId] = Uize.copy (
+											_inheritedLocaleStrings,
+											_moduleLocaleStrings
+										)
+									);
+								}
+								if (_module.mLoc_hasLoc) {
+									Uize.require (
+										_moduleName.replace (/\.Widget$/,'.Loc.' + _locale.replace ('-','_')),
+										_setLocaleStringsAndProvide
+									);
+								} else {
+									_setLocaleStringsAndProvide ();
+								}
+							}
+						);
+					}
+				} else {
+					_callback ({});
+				}
+			}
+
+		return function (_class) {
+			_class.declare ({
+				instanceMethods:{
+					loc:function (_resourceId,_inputs) {
+						return this.get ('loc_' + _resourceId) (_inputs);
+					}
+				},
+
+				treeInheritedStateProperties:{
+					locale:{value:'en-US'}
+				},
+
+				staticMethods:{
+					hasLoc:function (_hasLoc) {
+						this.nonInheritableStatics.mLoc_hasLoc = 1;
+						this.mLoc_hasLoc = _hasLoc;
+					}
+				},
+
+				stateProperties:{
+					localeInherited:{
+						onChange:function () {
+							var m = this;
+							_needLocaleStrings (m.Class,m.localeInherited,function (_localeStrings) {m.set (_localeStrings)});
+						}
+					}
+				}
+			});
+		};
+	}
+});
+
