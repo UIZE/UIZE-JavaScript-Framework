fa5a709faa20ecf3f7b6abc71b0ec375c0288cf3
Ben Ilegbodu <ben.ilegbodu@zazzle.com>
2014-04-10 18:22:03 -0700

    comment update

--- site-source/js/Uize/Test/Uize/Widget/mDeclarativeChildren.js
+++ site-source/js/Uize/Test/Uize/Widget/mDeclarativeChildren.js
@@ -1,7 +1,7 @@
 /*______________
 |       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
 |     /      /  |   ---------------------------------------------------
-|    /    O /   |    MODULE : Uize.Test.Uize.Widget.mCssBindings Class
+|    /    O /   |    MODULE : Uize.Test.Uize.Widget.mDeclarativeChildren Class
 |   /    / /    |
 |  /    / /  /| |    ONLINE : http://www.uize.com
 | /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
