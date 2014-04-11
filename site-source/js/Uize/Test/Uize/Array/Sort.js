bb10a201b283f35dc3d4d9b3d6c9e4d474fab03d
Chris van Rensburg <git-uize@tomkidding.com>
2014-04-11 10:22:57 -0700

    Improved the semantics for test case titles (moving away from the redundant "Test that...").

--- site-source/js/Uize/Test/Uize/Array/Sort.js
+++ site-source/js/Uize/Test/Uize/Array/Sort.js
@@ -35,36 +35,36 @@ Uize.module ({
 				Uize.Test.staticMethodsTest ([
 					['Uize.Array.Sort.sortBy',
 						[
-							['Test that sorting an empty array produces an empty array',
+							['Sorting an empty array produces an empty array',
 								[[]],
 								[]
 							],
-							['Test that sorting an array with only one element returns that same array',
+							['Sorting an array with only one element returns that same array',
 								[[1]],
 								[1]
 							],
 							/*** test default ascending sort ***/
-								['Test that sorting an array of numbers without specifying a sort value mapper will sort the numbers in ascending order',
+								['Sorting an array of numbers without specifying a sort value mapper will sort the numbers in ascending order',
 									[[10,4,2,5.6,-3.222,0]],
 									[-3.222,0,2,4,5.6,10]
 								],
-								['Test that sorting an array of strings without specifying a sort value mapper will sort the strings in ascending ASCIIbetical order',
+								['Sorting an array of strings without specifying a sort value mapper will sort the strings in ascending ASCIIbetical order',
 									[['paul','Deborah','ANDREW','15','DEBEORAH','andrew','...','jack']],
 									['...','15','ANDREW','DEBEORAH','Deborah','andrew','jack','paul']
 								],
 
 							/*** test null or undefined as the sort value mapper ***/
-								['Test that specifying the value undefined for the sort value mapper results in a simple ascending order sort',
+								['Specifying the value undefined for the sort value mapper results in a simple ascending order sort',
 									[[10,4,2,5.6,-3.222,0]],
 									[-3.222,0,2,4,5.6,10]
 								],
-								['Test that specifying the value null for the sort value mapper results in a simple ascending order sort',
+								['Specifying the value null for the sort value mapper results in a simple ascending order sort',
 									[[10,4,2,5.6,-3.222,0]],
 									[-3.222,0,2,4,5.6,10]
 								],
 
 							/*** test support for sort value mapper expression ***/
-								['Test that specifying a string value for sort value mapper results in that string being treated as a sort value mapper expression, in which context the identifier "value" can be used to access the value for an element',
+								['Specifying a string value for sort value mapper results in that string being treated as a sort value mapper expression, in which context the identifier "value" can be used to access the value for an element',
 									[
 										[
 											{firstName:'paul'},
@@ -83,13 +83,13 @@ Uize.module ({
 										{firstName:'paul'}
 									]
 								],
-								['Test that a string sort value mapper expression can use the identifier "key" in order to form mapped values using the indexes of elements',
+								['A string sort value mapper expression can use the identifier "key" in order to form mapped values using the indexes of elements',
 									[[1,2,3,4,5,6,7],'100 - key'],
 									[7,6,5,4,3,2,1]
 								],
 
 							/*** test support for sort column number ***/
-								['Test that specifying a number value for sort value mapper results in a sort by element number for an array of arrays',
+								['Specifying a number value for sort value mapper results in a sort by element number for an array of arrays',
 									[
 										[
 											['Mac Book Pro 17"',2299],
@@ -106,50 +106,50 @@ Uize.module ({
 								],
 
 							/*** test support for orderINT parameter ***/
-								['Test that specifying the value 1 for the optional orderINT parameter produces an ascending order sort',
+								['Specifying the value 1 for the optional orderINT parameter produces an ascending order sort',
 									[[10,4,2,5.6,-3.222,0],null,1],
 									[-3.222,0,2,4,5.6,10]
 								],
-								['Test that specifying the value 0 for the optional orderINT parameter produces an ascending order sort',
+								['Specifying the value 0 for the optional orderINT parameter produces an ascending order sort',
 									[[10,4,2,5.6,-3.222,0],null,0],
 									[-3.222,0,2,4,5.6,10]
 								],
-								['Test that specifying the value null for the optional orderINT parameter produces an ascending order sort',
+								['Specifying the value null for the optional orderINT parameter produces an ascending order sort',
 									[[10,4,2,5.6,-3.222,0],null,null],
 									[-3.222,0,2,4,5.6,10]
 								],
-								['Test that specifying the value undefined for the optional orderINT parameter produces an ascending order sort',
+								['Specifying the value undefined for the optional orderINT parameter produces an ascending order sort',
 									[[10,4,2,5.6,-3.222,0],null,undefined],
 									[-3.222,0,2,4,5.6,10]
 								],
-								['Test that specifying the value -1 for the optional orderINT parameter produces a descending order sort',
+								['Specifying the value -1 for the optional orderINT parameter produces a descending order sort',
 									[[10,4,2,5.6,-3.222,0],null,-1],
 									[10,5.6,4,2,0,-3.222]
 								],
 
 							/*** test support for value mapper function ***/
-								['Test that specifying a sort value mapper function is handled correctly',
+								['When a sort value mapper function is specified, the values it returns are used to determine the order of elements in the sorted array',
 									[
 										['paul','Dian','DEBEORAH','andrew','jaCK'],
 										function (value) {return value.toLowerCase ()}
 									],
 									['andrew','DEBEORAH','Dian','jaCK','paul']
 								],
-								['Test that a sort value mapper function can use the optional second parameter in order to form mapped values using the indexes of elements',
+								['A sort value mapper function can use the optional second parameter in order to form mapped values using the indexes of elements',
 									[[1,2,3,4,5,6,7],function (value,key) {return 100 - key}],
 									[7,6,5,4,3,2,1]
 								],
 
 							/*** test that source array is modified ***/
 								{
-									title:'Test that sorting an array modifies the source array and does not create a new array for the case of no sort value mapper',
+									title:'Sorting an array modifies the source array and does not create a new array for the case of no sort value mapper',
 									test:function () {
 										var _sourceArray = ['paul','Dian','DEBEORAH','andrew','jaCK'];
 										return this.expect (true,_sourceArray == Uize.Array.Sort.sortBy (_sourceArray));
 									}
 								},
 								{
-									title:'Test that sorting an array modifies the source array and does not create a new array for the case of specifying a sort value mapper function',
+									title:'Sorting an array modifies the source array and does not create a new array for the case of specifying a sort value mapper function',
 									test:function () {
 										var _sourceArray = ['paul','Dian','DEBEORAH','andrew','jaCK'];
 										return this.expect (
--- site-source/js/Uize/Test/Uize/Data/Util.js
+++ site-source/js/Uize/Test/Uize/Data/Util.js
@@ -35,93 +35,93 @@ Uize.module ({
 				Uize.Test.requiredModulesTest ('Uize.Data.Util'),
 				Uize.Test.staticMethodsTest ([
 					['Uize.Data.Util.filter',[
-						['Test that calling with no parameter produces an empty object',
+						['Calling with no parameter produces an empty object',
 							[],
 							{}
 						],
-						['Test that calling with the value null specified for source object produces an empty object',
+						['Calling with the value null specified for source object produces an empty object',
 							[null],
 							{}
 						],
-						['Test that calling with the value undefined specified for source object produces an empty object',
+						['Calling with the value undefined specified for source object produces an empty object',
 							[undefined],
 							{}
 						],
-						['Test that calling with no propertyNames parameter produces an empty object',
+						['Calling with no propertyNames parameter produces an empty object',
 							[{foo:1,bar:2,fooBar:3}],
 							{}
 						],
-						['Test that calling with null specified for propertyNames parameter produces an empty object',
+						['Calling with null specified for propertyNames parameter produces an empty object',
 							[{foo:1,bar:2,fooBar:3},null],
 							{}
 						],
-						['Test that calling with undefined specified for propertyNames parameter produces an empty object',
+						['Calling with undefined specified for propertyNames parameter produces an empty object',
 							[{foo:1,bar:2,fooBar:3},undefined],
 							{}
 						],
-						['Test that calling with empty array specified for propertyNames parameter produces an empty object',
+						['Calling with empty array specified for propertyNames parameter produces an empty object',
 							[{foo:1,bar:2,fooBar:3},[]],
 							{}
 						],
-						['Test that calling with propertyNames array with no matching properties produces an empty object',
+						['Calling with propertyNames array with no matching properties produces an empty object',
 							[{foo:1,bar:2,fooBar:3},['hello','kitty']],
 							{}
 						],
-						['Test that calling with propertyNames array with superset of properties produces source object',
+						['Calling with propertyNames array with superset of properties produces source object',
 							[{foo:1,bar:2,fooBar:3},['foo','bar','fooBar','hello','kitty']],
 							{foo:1,bar:2,fooBar:3}
 						],
-						['Test that calling with propertyNames array with subset of properties produces subset source object',
+						['Calling with propertyNames array with subset of properties produces subset source object',
 							[{foo:1,bar:2,fooBar:3},['foo','bar','hello','kitty']],
 							{foo:1,bar:2}
 						],
-						['Test that calling with empty source object produces empty object',
+						['Calling with empty source object produces empty object',
 							[{},['foo','bar','hello','kitty']],
 							{}
 						],
-						['Test that source object properties with values undefined, null, 0, NaN, or false are not missed',
+						['Source object properties with values undefined, null, 0, NaN, or false are not missed',
 							[{foo:undefined,bar:null,fooBar:0,hello:NaN,kitty:false},['foo','bar','fooBar','hello','kitty']],
 							{foo:undefined,bar:null,fooBar:0,hello:NaN,kitty:false}
 						],
-						['Test that duplicate values in propertyNames array are ok',
+						['Duplicate values in propertyNames array are ok',
 							[{foo:1,bar:2,fooBar:3},['foo','foo','bar','bar']],
 							{foo:1,bar:2}
 						]
 					]],
 					['Uize.Data.Util.findRecords',[
-						['Test that calling with no parameter produces an empty array',
+						['Calling with no parameter produces an empty array',
 							[],
 							[]
 						],
-						['Test that calling with null for records array produces an empty array',
+						['Calling with null for records array produces an empty array',
 							[null,{type:'b'}],
 							[]
 						],
-						['Test that calling with undefined for records array produces an empty array',
+						['Calling with undefined for records array produces an empty array',
 							[null,{type:'b'}],
 							[]
 						],
-						['Test that calling with an empty records array produces an empty array',
+						['Calling with an empty records array produces an empty array',
 							[[],{type:'b'}],
 							[]
 						],
-						['Test that calling with a non-empty records array with no matching records produces an empty array',
+						['Calling with a non-empty records array with no matching records produces an empty array',
 							[[{foo:1,type:'a'},{bar:2,type:'c'}],{type:'b'}],
 							[]
 						],
-						['Test that calling with a non-empty records array and no match specified produces source array',
+						['Calling with a non-empty records array and no match specified produces source array',
 							[[{foo:1},{bar:2}]],
 							[{foo:1},{bar:2}]
 						],
-						['Test that calling with a non-empty records array and null for match produces source array',
+						['Calling with a non-empty records array and null for match produces source array',
 							[[{foo:1},{bar:2}],null],
 							[{foo:1},{bar:2}]
 						],
-						['Test that calling with a non-empty records array and undefined for match produces source array',
+						['Calling with a non-empty records array and undefined for match produces source array',
 							[[{foo:1},{bar:2}],undefined],
 							[{foo:1},{bar:2}]
 						],
-						['Test that calling with a non-empty records array with matching records produces array with those matching records, in the correct order',
+						['Calling with a non-empty records array with matching records produces array with those matching records, in the correct order',
 							[[{foo:1,type:'a'},{hello:3,type:'b'},{bar:2,type:'c'},{kitty:4,type:'b'}],{type:'b'}],
 							[{hello:3,type:'b'},{kitty:4,type:'b'}]
 						]
