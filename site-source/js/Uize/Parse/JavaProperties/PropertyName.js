5e3fd3f5bb750e84a5926727a3014bd223181fa3
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2014-04-30 18:57:05 -0700

    Created a parser object for property names in Java properties files.

---
+++ site-source/js/Uize/Parse/JavaProperties/PropertyName.js
@@ -0,0 +1,105 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Parse.JavaProperties.PropertyName Object
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Object
+	importance: 1
+	codeCompleteness: 1
+	docCompleteness: 2
+*/
+
+/*?
+	Introduction
+		The =Uize.Parse.JavaProperties.PropertyName= module provides methods for parsing and serializing property names in [[http://en.wikipedia.org/wiki/.properties][Java properties]] files.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Parse.JavaProperties.PropertyName',
+	required:[
+		'Uize.Str.Whitespace',
+		'Uize.Str.Replace',
+		'Uize.Parse.JavaProperties.UnicodeEscaped'
+	],
+	builder:function () {
+		'use strict';
+
+		var
+			/*** Variables for Performance Optimization ***/
+				_isNonWhitespace = Uize.Str.Whitespace.isNonWhitespace,
+
+			/*** General Variables ***/
+				_terminatingCharsLookup = _charsLookup (' :='),
+				_serializerEscaper = Uize.Str.Replace.replacerByLookup ({
+					'\\':'\\\\',
+					':':'\\:',
+					'=':'\\=',
+					' ':'\\ '
+				})
+		;
+
+		/*** Utility Functions ***/
+			function _charsLookup (_charsStr) {
+				return Uize.lookup (_charsStr.split (''));
+			}
+
+		return Uize.mergeInto (
+			function (_source,_index) {
+				this.parse (_source,_index);
+			},
+			{
+				prototype:{
+					source:'',
+					index:0,
+					length:0,
+					isValid:false,
+					name:'',
+
+					parse:function (_source,_index) {
+						var
+							m = this,
+							_sourceLength = (m.source = _source = _source || '').length
+						;
+						m.index = _index || (_index = 0);
+						m.isValid = false;
+						if (_isNonWhitespace (_source.charAt (_index))) {
+							m.isValid = true;
+							_index++;
+							var
+								_inEscape = false,
+								_char
+							;
+							while (
+								_index < _sourceLength &&
+								(_inEscape || !_terminatingCharsLookup [_char = _source.charAt (_index)])
+							) {
+								_inEscape = !_inEscape && _char == '\\';
+								_index++;
+							}
+							m.name = _source.slice (m.index,_index).replace (/\\(.)/g,'$1');
+							m.length = _index - m.index;
+						}
+					},
+
+					serialize:function () {
+						return (
+							this.isValid
+								? Uize.Parse.JavaProperties.UnicodeEscaped.to (_serializerEscaper (this.name))
+								: ''
+						);
+					}
+				}
+			}
+		);
+	}
+});
+
---
+++ site-source/js/Uize/Test/Uize/Parse/JavaProperties/PropertyName.js
@@ -0,0 +1,150 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Test.Uize.Parse.JavaProperties.PropertyName Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Test
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Test.Uize.Parse.JavaProperties.PropertyName= module defines a suite of unit tests for the =Uize.Parse.JavaProperties.PropertyName= module.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Test.Uize.Parse.JavaProperties.PropertyName',
+	superclass:'Uize.Test.ParserTest',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			staticProperties:{parserClass:'Uize.Parse.JavaProperties.PropertyName'},
+
+			set:{
+				title:'Test for Uize.Parse.JavaProperties.PropertyName Module',
+				test:[
+					Uize.Test.requiredModulesTest ('Uize.Parse.JavaProperties.PropertyName'),
+					{
+						title:'A property name can be parsed',
+						test:[
+							Uize.Test.ParserTest.parserTest (
+								'A property name may be a single word containing alphabetical characters',
+								['Property'],
+								{
+									name:'Property',
+									isValid:true
+								}
+							),
+							Uize.Test.ParserTest.parserTest (
+								'A property name may contain digits',
+								['Property0123456789'],
+								{
+									name:'Property0123456789',
+									isValid:true
+								}
+							),
+							Uize.Test.ParserTest.parserTest (
+								'A property name may unescaped special characters that are not ":" or "="',
+								['Property~!@#$%^&*()_+-`[]{}|;".,/?'],
+								{
+									name:'Property~!@#$%^&*()_+-`[]{}|;".,/?',
+									isValid:true
+								}
+							),
+							Uize.Test.ParserTest.parserTest (
+								'Parsing of a property name is terminated by a ":" (colon) character',
+								['Property:Value'],
+								{
+									name:'Property',
+									isValid:true
+								}
+							),
+							Uize.Test.ParserTest.parserTest (
+								'Parsing of a property name is terminated by a "=" (equals) character',
+								['Property=Value'],
+								{
+									name:'Property',
+									isValid:true
+								}
+							),
+							Uize.Test.ParserTest.parserTest (
+								'Parsing of a property name is terminated by a " " (space) character',
+								['Property Value'],
+								{
+									name:'Property',
+									isValid:true
+								}
+							),
+							Uize.Test.ParserTest.parserTest (
+								'A property name may contain ":" (colon), "=" (equals), or " " (space) characters, as long as they are escaped with a "\" (backslash) character',
+								['Foo\\:Bar\\=Baz\\ Qux=Value'],
+								{
+									name:'Foo:Bar=Baz Qux',
+									isValid:true
+								}
+							),
+							Uize.Test.ParserTest.parserTest (
+								'A property name may not start with whitespace',
+								[' Property'],
+								{
+									name:'',
+									isValid:false
+								}
+							)
+						]
+					},
+					{
+						title:'A property name can be serialized',
+						test:[
+							Uize.Test.ParserTest.serializerTest (
+								'A property name that is a single word containing alphabetical characters can be serialized',
+								{
+									name:'Property',
+									isValid:true
+								},
+								'Property'
+							),
+							Uize.Test.ParserTest.serializerTest (
+								'A property name containing digits can be serialized',
+								{
+									name:'Property0123456789',
+									isValid:true
+								},
+								'Property0123456789'
+							),
+							Uize.Test.ParserTest.serializerTest (
+								'A property name containing special characters that are not ":" (colon), "=" (equals), or " " (space) can be serialized',
+								{
+									name:'Property~!@#$%^&*()_+-`[]{}|;".,/?',
+									isValid:true
+								},
+								'Property~!@#$%^&*()_+-`[]{}|;".,/?'
+							),
+							Uize.Test.ParserTest.serializerTest (
+								'When a property contains ":" (colon), "=" (equals), or " " (space) characters, they are escaped with a "\" (backslash) character when the property name is serizlied',
+								{
+									name:'Foo:Bar=Baz Qux',
+									isValid:true
+								},
+								'Foo\\:Bar\\=Baz\\ Qux'
+							)
+						]
+					}
+				]
+			}
+		});
+	}
+});
+
