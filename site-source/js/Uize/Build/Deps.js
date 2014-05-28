82fbcb83d36699e7ae1a38deacb1aabd57547266
Chris van Rensburg <git-uize@tomkidding.com>
2014-05-28 09:38:38 -0700

    More progress with the dependency analysis build script.

--- site-source/js/Uize/Build/Deps.js
+++ site-source/js/Uize/Build/Deps.js
@@ -30,14 +30,68 @@ Uize.module ({
 		'Uize.Build.Files',
 		'Uize.Build.ModuleInfo',
 		'Uize.Services.FileSystem',
-		'Uize.Json'
+		'Uize.Json',
+		'Uize.Data.Util',
+		'Uize.Str.Repeat'
 	],
 	builder:function () {
 		'use strict';
 
 		/*** Utility Functions ***/
 			function _renderTextTable (_params) {
-				return Uize.Json.to (_params.rows);
+				var
+					_columns = _params.columns,
+					_rows = _params.rows
+				;
+
+				/*** resolve column information ***/
+					Uize.forEach (
+						_columns,
+						function (_column,_columnNo) {
+							var _columnValues = Uize.Data.Util.getColumn (_rows,_columnNo);
+							if (!('minValue' in _column)) _column.minValue = Uize.min (_columnValues);
+							if (!('maxValue' in _column)) _column.maxValue = Uize.max (_columnValues);
+							_column.formatter = Uize.resolveTransformer (_column.formatter);
+						}
+					);
+
+				/*** format column data and get max width for each column ***/
+					var
+						_columnMaxWidths = [],
+						_formattedRows = Uize.map (
+							_rows,
+							function (_row) {
+								return Uize.map (
+									_columns,
+									function (_column,_columnNo) {
+										var _formattedValue = _column.formatter (_row [_columnNo]) + '';
+										_columnMaxWidths [_columnNo] = Math.max (
+											_columnMaxWidths [_columnNo] || 0,
+											_formattedValue.length
+										);
+										return _formattedValue;
+									}
+								);
+							}
+						)
+					;
+
+				return Uize.map (
+					_formattedRows,
+					function (_row) {
+						return (
+							'| ' +
+							Uize.map (
+								_row,
+								function (_column,_columnNo) {
+									var _padding = Uize.Str.Repeat.repeat (' ',_columnMaxWidths [_columnNo] - _column.length);
+									return _columns [_columnNo].align == 'right' ? _padding + _column : _column + _padding;
+								}
+							).join (' | ') +
+							' |'
+						);
+					}
+				).join ('\n');
 			}
 
 		return Uize.package ({
@@ -74,42 +128,61 @@ Uize.module ({
 							_renderTextTable ({
 								title:'Complete List of Dependencies',
 								columns:[
-									{title:'Module Name'},
-									{title:'Type'},
-									{title:'Order'},
+									{
+										title:'Module Name',
+										align:'left'
+									},
+									{
+										title:'Type',
+										align:'left'
+									},
+									{
+										title:'Order',
+										align:'right'
+									},
 									{
 										title:'Depth',
+										align:'right',
 										minValue:0,
 										maxValue:2
 									},
 									{
 										title:'Import.',
+										align:'right',
 										minValue:0,
 										maxValue:10
 									},
 									{
 										title:'Code',
+										align:'right',
 										minValue:0,
 										maxValue:100,
 										formatter:'value + "%"'
 									},
-									{title:'Dir. Deps'},
+									{
+										title:'Dir. Deps',
+										align:'right',
+									},
 									{
 										title:'Shared',
+										align:'right',
 										minValue:0
 									},
 									{
 										title:'Size (%)',
+										align:'right',
 										minValue:0,
 										maxValue:100,
 										formatter:'value.toFixed (1) + "%"'
 									},
 									{
 										title:'Size (B)',
+										align:'right',
 										formatter:'value + " B"'
 									},
 									{
 										title:'Unique',
+										align:'right',
 										formatter:'value + " B"'
 									}
 								],
