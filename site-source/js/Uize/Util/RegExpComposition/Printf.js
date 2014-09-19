8d6ab59ef428ef691473437b0f9358d2c7bc3fe5
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2014-09-19 14:59:11 -0700

    A basic utility regular expression composition for printf format specifier character sequences that can be used in any parsing situations where it might be useful.

---
+++ site-source/js/Uize/Util/RegExpComposition/Printf.js
@@ -0,0 +1,41 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Util.RegExpComposition.Printf Package
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2013-2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Object
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 4
+*/
+
+/*?
+	Introduction
+		The =Uize.Util.RegExpComposition.Printf= package defines a regular expression composition object for matching [[http://pubs.opengroup.org/onlinepubs/009695399/functions/printf.html][printf formatting specifier]] character sequences.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Util.RegExpComposition.Printf',
+	required:'Uize.Util.RegExpComposition',
+	builder:function () {
+		'use strict';
+
+		return Uize.Util.RegExpComposition ({
+			code:/[@%dDuUxXoOfeEgGcCsSPaAF]/,
+			lengthModifier:/hh?|ll?|[qLztj]/,
+			lengthModifierAndCode:/(?:{lengthModifier})?{code}/,
+			specifier:/%{lengthModifierAndCode}/,
+			specifierWithCapture:/%({lengthModifierAndCode})/
+		});
+	}
+});
+
