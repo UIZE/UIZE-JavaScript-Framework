547304f6c735fe06c284b61c30d7df6440150df4
Chris van Rensburg <git-uize@tomkidding.com>
2014-09-22 10:55:53 -0700

    Created a new reset button which, like the clear button, is to have its own localized label strings.

---
+++ site-source/js/Uize/Widgets/Buttons/Reset/VisualSampler.js
@@ -0,0 +1,55 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Buttons.Reset.VisualSampler Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Buttons.Reset.VisualSampler= class implements a visual sampler widget for the =Uize.Widgets.Buttons.Reset.Widget= class.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Buttons.Reset.VisualSampler',
+	superclass:'Uize.Widgets.VisualSampler.Widget',
+	required:[
+		'Uize.Widgets.Buttons.Reset.Widget',
+		'Uize.Widgets.StateValues'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			omegastructor:function () {
+				this.addStateCombinationSamples ({
+					locale:Uize.Widgets.StateValues.locale
+				});
+				this.addStateCombinationSamples ({
+					flavor:['normal','negative','positive','primary']
+				});
+				this.addStateCombinationSamples ({
+					size:Uize.Widgets.StateValues.size
+				});
+			},
+
+			set:{
+				samplerWidgetClass:Uize.Widgets.Buttons.Reset.Widget
+			}
+		});
+	}
+});
+
---
+++ site-source/js/Uize/Widgets/Buttons/Reset/Widget.js
@@ -0,0 +1,50 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Buttons.Reset.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Buttons.Reset.Widget= class implements a localized reset button.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+
+		Visual Sampler
+			Below is a visual sampler of the =Uize.Widgets.Buttons.Reset.Widget= class...
+
+			.....................................................
+			<< widget >>
+
+			widgetClass: Uize.Widgets.Buttons.Reset.VisualSampler
+			.....................................................
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Buttons.Reset.Widget',
+	superclass:'Uize.Widgets.Button.Widget',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			hasLoc:true,
+
+			stateProperties:{
+				text:{derived:'loc_label'}
+			}
+		});
+	}
+});
+
