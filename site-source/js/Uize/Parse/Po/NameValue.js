89cf4c1cc04e4f8739949d86109ba0126e153b11
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2014-10-06 17:38:09 -0700

    A parser module for name-value pairs inside GNU gettext PO files.

---
+++ site-source/js/Uize/Parse/Po/NameValue.js
@@ -0,0 +1,89 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Parse.Po.NameValue Object
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Object
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 2
+*/
+
+/*?
+	Introduction
+		The =Uize.Parse.Po.NameValue= module provides methods for parsing and serializing name-value pairs in GNU gettext [[https://www.gnu.org/software/gettext/manual/html_node/PO-Files.html][PO files]].
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Parse.Po.NameValue',
+	required:[
+		'Uize.Str.Whitespace',
+		'Uize.Str.CharClass',
+		'Uize.Parse.Po.Name',
+		'Uize.Parse.Po.Value',
+	],
+	builder:function () {
+		'use strict';
+
+		var
+			/*** Variables for Performance Optimization ***/
+				_Uize_Parse_Po_Name = Uize.Parse.Po.Name,
+				_Uize_Parse_Po_Value = Uize.Parse.Po.Value,
+
+			/*** General Variables ***/
+				_inlineWhitespaceCharClass = Uize.Str.CharClass (Uize.Str.Whitespace.inlineWhitespaceCharCodes)
+		;
+
+		return Uize.mergeInto (
+			function (_source,_index) {
+				var m = this;
+				m.name = new _Uize_Parse_Po_Name;
+				m.value = new _Uize_Parse_Po_Value;
+				this.parse (_source,_index);
+			},
+			{
+				prototype:{
+					source:'',
+					index:0,
+					length:0,
+					isValid:false,
+
+					parse:function (_source,_index) {
+						function _eatInlineWhitespace () {
+							_index =
+								(_inlineWhitespaceCharClass.indexOfNonClassChar (_source,_index) + 1 || _sourceLength + 1) - 1
+							;
+						}
+						var
+							m = this,
+							_sourceLength = (m.source = _source = _source || '').length
+						;
+						m.index = _index || (_index = 0);
+						m.name.parse (_source,_index);
+						if (m.name.isValid) {
+							_index += m.name.length;
+							_eatInlineWhitespace ();
+							m.value.parse (_source,_index);
+							_index = m.value.isValid ? _index + m.value.length : m.index;
+						}
+						m.isValid = !!(m.length = _index - m.index);
+					},
+
+					serialize:function () {
+						return this.isValid ? this.name.serialize () + ' ' + this.value.serialize () : '';
+					}
+				}
+			}
+		);
+	}
+});
+
---
+++ site-source/js/Uize/Test/Uize/Parse/Po/NameValue.js
@@ -0,0 +1,104 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Test.Uize.Parse.Po.NameValue Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Test
+	importance: 1
+	codeCompleteness: 80
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Test.Uize.Parse.Po.NameValue= module defines a suite of unit tests for the =Uize.Parse.Po.NameValue= module.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Test.Uize.Parse.Po.NameValue',
+	superclass:'Uize.Test.ParserTest',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			staticProperties:{parserClass:'Uize.Parse.Po.NameValue'},
+
+			set:{
+				title:'Test for Uize.Parse.Po.NameValue Module',
+				test:[
+					Uize.Test.requiredModulesTest ('Uize.Parse.Po.NameValue'),
+					{
+						title:'A name-value pair can be parsed',
+						test:[
+							Uize.Test.ParserTest.parserTest (
+								'The name and value are separated by an arbitrary amount of inline whitespace',
+								['msgid "foo"'],
+								{
+									name:{
+										name:'msgid',
+										isValid:true
+									},
+									value:{
+										value:'foo',
+										isValid:true
+									},
+									isValid:true
+								}
+							),
+							Uize.Test.ParserTest.parserTest (
+								'The value in a name-value pair must start on the same line as the name',
+								['msgid \n"foo"'],
+								{isValid:false}
+							),
+							Uize.Test.ParserTest.parserTest (
+								'The value in a name-value pair may span multiple lines, if each part is enclosed in quotes',
+								['msgid "foo"\n" bar"\n" baz"\n\n\r" qux"'],
+								{
+									name:{
+										name:'msgid',
+										isValid:true
+									},
+									value:{
+										value:'foo bar baz qux',
+										isValid:true
+									},
+									isValid:true
+								}
+							)
+						]
+					},
+					{
+						title:'A name-value pair can be serialized',
+						test:[
+							Uize.Test.ParserTest.serializerTest (
+								'When a name-value pair is serialized, a single space is used as the name-value separator',
+								{
+									name:{
+										name:'msgid',
+										isValid:true
+									},
+									value:{
+										value:'foo',
+										isValid:true
+									},
+									isValid:true
+								},
+								'msgid "foo"'
+							)
+						]
+					}
+				]
+			}
+		});
+	}
+});
+
