c95cd71e06cdee3641817210d7fdc906631f2da3
Chris van Rensburg <git-uize@tomkidding.com>
2014-10-07 09:53:29 -0700

    Added a reset button to the shift-and-range widget.

--- site-source/js/Uize/Widgets/ColorsTransformer/ShiftAndRange/Widget.js
+++ site-source/js/Uize/Widgets/ColorsTransformer/ShiftAndRange/Widget.js
@@ -32,11 +32,19 @@
 			.......................................................................
 */
 
+/* TODO:
+	- factor out commonalities in CSS between this widget and the log widget
+		- centered title
+		- title state property and binding to HTML
+		- heading styling to accommodate top right button
+*/
+
 Uize.module ({
 	name:'Uize.Widgets.ColorsTransformer.ShiftAndRange.Widget',
 	superclass:'Uize.Widgets.BoxWithHeading.Widget',
 	required:[
 		'Uize.Widgets.Slider.Widget',
+		'Uize.Widgets.Buttons.Reset.Widget',
 		'Uize.Widgets.ColorsTransformer.ShiftAndRange.Html',
 		'Uize.Widgets.ColorsTransformer.ShiftAndRange.Css',
 		'Uize.Widget.mChildBindings'
@@ -71,25 +79,38 @@ Uize.module ({
 					_shiftSliderMaxValueLabel:{derived:'shiftExtent: "+" + shiftExtent'}
 			},
 
-			children:Uize.map (
-				{
-					shiftSlider:{},
-					rangeSlider:{
-						minValue:0,
-						maxValue:100
-					}
-				},
-				function (_properties) {
-					return Uize.copyInto (
-						_properties,
-						{
-							widgetClass:Uize.Widgets.Slider.Widget,
-							increments:0,
-							orientation:'horizontal',
-							fullColor:'#fff',
-							emptyColor:'#fff'
+			children:Uize.copyInto (
+				Uize.map (
+					{
+						shiftSlider:{},
+						rangeSlider:{
+							minValue:0,
+							maxValue:100
 						}
-					);
+					},
+					function (_properties) {
+						return Uize.copyInto (
+							_properties,
+							{
+								widgetClass:Uize.Widgets.Slider.Widget,
+								increments:0,
+								orientation:'horizontal',
+								fullColor:'#fff',
+								emptyColor:'#fff'
+							}
+						);
+					}
+				),
+				{
+					reset:{
+						widgetClass:Uize.Widgets.Buttons.Reset.Widget,
+						action:function () {
+							this.parent.set ({
+								shift:0,
+								range:50
+							});
+						}
+					}
 				}
 			),
 
@@ -105,7 +126,7 @@ Uize.module ({
 			},
 
 			htmlBindings:{
-				heading:'heading:html',
+				heading:'title',
 				loc_shift:'shiftLabel',
 				loc_range:'rangeLabel',
 				_shiftSliderMinValueLabel:'shiftMinValue',
