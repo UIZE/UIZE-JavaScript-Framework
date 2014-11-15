c3430702a6425163bf81d8168583cbf019b8273c
Chris van Rensburg <git-uize@tomkidding.com>
2014-11-15 11:17:08 -0800

    An encapsulated image port widget.

---
+++ site-source/js/Uize/Widgets/ImagePort/VisualSampler.js
@@ -0,0 +1,53 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.ImagePort.VisualSampler Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.ImagePort.VisualSampler= class implements a visual sampler widget for the =Uize.Widgets.ImagePort.Widget= class.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.ImagePort.VisualSampler',
+	superclass:'Uize.Widgets.VisualSampler.Widget',
+	required:'Uize.Widgets.ImagePort.Widget',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			omegastructor:function () {
+				this.addSample ({
+					alignX:.5,
+					alignY:.5,
+					sizingLowerBound:'fit',
+					sizingUpperBound:'fill',
+					sizingValue:1,
+					width:700,
+					height:500,
+					imageUrl:Uize.pathToResources + Uize.modulePathResolver ('Uize.Widgets.ImagePort.TestAssets') + '/uize-50x50.gif'
+				});
+			},
+
+			set:{
+				samplerWidgetClass:Uize.Widgets.ImagePort.Widget
+			}
+		});
+	}
+});
+
---
+++ site-source/js/Uize/Widgets/ImagePort/Widget.js
@@ -0,0 +1,71 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.ImagePort.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.ImagePort.Widget= module implements a widget class.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+
+		Visual Sampler
+			Below is a visual sampler of the =Uize.Widgets.ImagePort.Widget= class...
+
+			..................................................
+			<< widget >>
+
+			widgetClass: Uize.Widgets.ImagePort.VisualSampler
+			..................................................
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.ImagePort.Widget',
+	superclass:'Uize.Widget.ImagePort',
+	required:[
+		'Uize.Widget.mV2',
+		'Uize.Widget.mWidthHeight',
+		'Uize.Widgets.ImagePort.Html',
+		'Uize.Widgets.ImagePort.Css'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			mixins:[
+				Uize.Widget.mV2,
+				Uize.Widget.mWidthHeight
+			],
+
+			set:{
+				html:Uize.Widgets.ImagePort.Html
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.ImagePort.Css
+			},
+
+			stateProperties:{
+				imageUrl:{}
+			},
+
+			htmlBindings:{
+				imageUrl:'image:src'
+			}
+		});
+	}
+});
+
