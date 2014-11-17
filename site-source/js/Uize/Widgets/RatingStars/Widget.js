952a890f21e441282f48463033feda21e272e0e2
Chris van Rensburg <git-uize@tomkidding.com>
2014-11-16 21:30:54 -0800

    Some basic docs for the rating stars widget.

--- site-source/js/Uize/Widgets/RatingStars/Widget.js
+++ site-source/js/Uize/Widgets/RatingStars/Widget.js
@@ -22,6 +22,22 @@
 
 		*DEVELOPERS:* `Chris van Rensburg`
 
+		In a Nutshell
+			The =Uize.Widgets.ImagePort.Widget= widget module supports a very simple interface with two state properties: =value= and =maxValue=.
+
+			The =maxValue= state property is used to specify the maximum possible number of stars that a rating can have, and the =value= state property is used to specify the number of rating stars for the current rating value.
+
+			EXAMPLE
+			....................................................
+			var ratingStars = Uize.Widgets.RatingStars.Widget ({
+				value:2.5,
+				maxValue:5,
+				size:'tiny'
+			});
+			....................................................
+
+			In the above example, a rating stars widget is being created to display a two and half stars out of five rating at the ='tiny'= size.
+
 		Visual Sampler
 			Below is a visual sampler of the =Uize.Widgets.RatingStars.Widget= class...
 
@@ -52,8 +68,34 @@ Uize.module ({
 			},
 
 			stateProperties:{
-				value:{value:3.5},
-				maxValue:{value:5},
+				value:{
+					value:0
+					/*?
+						State Properties
+							value
+								A number, in the range of =0= to the value of the =maxValue= state property, indicating the number of rating stars.
+
+								The value of this property may be fractional. So, for example, it is perfectly acceptable for the rating value to be =3.25= stars out of =5=.
+
+								NOTES
+								- see the related =maxValue= state property
+								- the initial value is =0=
+					*/
+				},
+				maxValue:{
+					value:5
+					/*?
+						State Properties
+							maxValue
+								A positive integer, indicating the maximum number of rating stars that are possible.
+
+								The value of this property must be an integer. So, for example, it is not acceptable for the maximum number of rating stars to be =5.5=.
+
+								NOTES
+								- see the related =value= state property
+								- the initial value is =5=
+					*/
+				},
 
 				/*** derived properties ***/
 					_tooltipText:{
