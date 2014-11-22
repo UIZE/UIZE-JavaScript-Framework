2ee8f1546fc65dc60c3b4377c81a8e2d668cc722
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2014-11-21 17:38:21 -0800

    A simple module to deal with strings that need to have backslash-escaped linebreaks, but no more sophisticated escaping than that.

---
+++ site-source/js/Uize/Str/BackslashEscapedLinebreaks.js
@@ -0,0 +1,73 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Str.BackslashEscapedLinebreaks Package
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Package
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 5
+*/
+
+/*?
+	Introduction
+		The =Uize.Str.BackslashEscapedLinebreaks= module provides methods for converting to and from backslash-escaped linebreaks.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Str.BackslashEscapedLinebreaks',
+	required:'Uize.Str.Replace',
+	builder:function () {
+		'use strict';
+
+		var
+			/*** General Variables ***/
+				_escaper = Uize.Str.Replace.replacerByLookup ({
+					'\\':'\\\\',
+					'\r':'\\r',
+					'\n':'\\n'
+				}),
+
+				_linebreakCodeToLinebreakLookup = {
+					n:'\n',  // new line
+					r:'\r'   // carriage return
+				}
+		;
+
+		return Uize.package ({
+			from:function (_toDecode) {
+				return (
+					_toDecode &&
+					_toDecode.replace (
+						/(\\*)([^\\]|$)/g,
+						function (_match,_backslashes,_charAfterBackslashes) {
+							var _backslashesLength = _backslashes.length;
+							return (
+								_backslashes.slice (0,Math.floor (_backslashesLength / 2)) +
+								(
+									(_backslashesLength % 2)
+										? _linebreakCodeToLinebreakLookup [_charAfterBackslashes] || _charAfterBackslashes
+										: _charAfterBackslashes
+								)
+							);
+						}
+					)
+				);
+			},
+
+			to:function (_toEncode) {
+				return _toEncode && _escaper (_toEncode);
+			}
+		});
+	}
+});
+
---
+++ site-source/js/Uize/Test/Uize/Str/BackslashEscapedLinebreaks.js
@@ -0,0 +1,89 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Test.Uize.Str.BackslashEscapedLinebreaks Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Test
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Test.Uize.Str.BackslashEscapedLinebreaks= module defines a suite of unit tests for the =Uize.Str.BackslashEscapedLinebreaks= module.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Test.Uize.Str.BackslashEscapedLinebreaks',
+	builder:function () {
+		'use strict';
+
+		return Uize.Test.resolve ({
+			title:'Uize.Str.BackslashEscapedLinebreaks Module Test',
+			test:[
+				Uize.Test.requiredModulesTest ('Uize.Str.BackslashEscapedLinebreaks'),
+				Uize.Test.staticMethodsTest ([
+					['Uize.Str.BackslashEscapedLinebreaks.from',[
+						['A backslash-escaped new line at the start of the string is unescaped to a new line character',
+							'\\nfoo bar',
+							'\nfoo bar'
+						],
+						['A backslash-escaped new line at the end of the string is unescaped to a new line character',
+							'foo bar\\n',
+							'foo bar\n'
+						],
+						['A backslash-escaped new line in the middle of the string is unescaped to a new line character',
+							'foo\\nbar',
+							'foo\nbar'
+						],
+						['A backslash-escaped carriage return is unescaped to a carriage return character',
+							'\\rfoo\\rbar\\r',
+							'\rfoo\rbar\r'
+						],
+						['An "n" or "r" with an even number of preceding backslash characters is not escaped to a new line or carriage return, but is left as an "n" or "r" character, and the preceding backslash characters are treated as backslash-escaped backslashes',
+							'\\\\\\\\n \\\\\\\\r',
+							'\\\\n \\\\r'
+						],
+						['An "n" or "r" with an odd number of preceding backslash characters is escaped to a new line or carriage return, and the preceding backslash characters minus the immediately preceding backslash character are treated as backslash-escaped backslashes',
+							'\\\\\\\\\\n \\\\\\\\\\r',
+							'\\\\\n \\\\\r'
+						],
+						['A character following an escaping backslash but that is neither an "n" nor an "r" is left as is',
+							'\\\\\\.',
+							'\\.'
+						],
+						['An escaping backslash that is at the end of the string and that is not paired with an escape character code is ignored',
+							'foo\\',
+							'foo'
+						]
+					]],
+					['Uize.Str.BackslashEscapedLinebreaks.to',[
+						['New line characters are backslash-escaped',
+							'\nfoo\nbar\n',
+							'\\nfoo\\nbar\\n'
+						],
+						['Carriage return characters are backslash-escaped',
+							'\rfoo\rbar\r',
+							'\\rfoo\\rbar\\r'
+						],
+						['Backslash characters are backslash-escaped',
+							'\\red \\\\nut',
+							'\\\\red \\\\\\\\nut'
+						]
+					]]
+				])
+			]
+		});
+	}
+});
+
