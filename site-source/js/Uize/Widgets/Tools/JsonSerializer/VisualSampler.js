d6cb1b29f4b09de60cf6709d2dff53d5fb50a97d
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2014-12-22 19:09:00 -0800

    Progress with the new JSON serializer widget.

---
+++ site-source/js/Uize/Widgets/Tools/JsonSerializer/VisualSampler.js
@@ -0,0 +1,46 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Tools.JsonSerializer.VisualSampler Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 5
+	docCompleteness: 100
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Tools.JsonSerializer.VisualSampler= class implements a visual sampler widget for the =Uize.Widgets.Tools.JsonSerializer.Widget= class.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Tools.JsonSerializer.VisualSampler',
+	superclass:'Uize.Widgets.VisualSampler.Widget',
+	required:'Uize.Widgets.Tools.JsonSerializer.Widget',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			omegastructor:function () {
+				this.addSample ({
+					source:'{helloThere:\'world\',foo:\'bar\'}'
+				});
+			},
+
+			set:{
+				samplerWidgetClass:Uize.Widgets.Tools.JsonSerializer.Widget
+			}
+		});
+	}
+});
+
---
+++ site-source/js/Uize/Widgets/Tools/JsonSerializer/Widget.js
@@ -0,0 +1,144 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Tools.JsonSerializer.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2014 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 5
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Tools.JsonSerializer.Widget= module implements a widget class for a JSON serializer tool.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+
+		Visual Sampler
+			Below is a visual sampler of the =Uize.Widgets.Tools.JsonSerializer.Widget= class...
+
+			............................................................
+			<< widget >>
+
+			widgetClass: Uize.Widgets.Tools.JsonSerializer.VisualSampler
+			............................................................
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Tools.JsonSerializer.Widget',
+	superclass:'Uize.Widget.V2',
+	required:[
+		'Uize.Widgets.Tools.JsonSerializer.Html',
+		'Uize.Widgets.Tools.JsonSerializer.Css',
+		'Uize.Widgets.Button.Toggle.OnOff.Widget',
+		'Uize.Widgets.Tools.SourceVsResult.Widget',
+		'Uize.Json'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		var
+			/*** Variables for Scruncher Optimization ***/
+				_onOffTogglerWidgetProperties = {widgetClass:Uize.Widgets.Button.Toggle.OnOff.Widget}
+		;
+
+		return _superclass.subclass ({
+			hasLoc:true,
+
+			set:{
+				html:Uize.Widgets.Tools.JsonSerializer.Html
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.Tools.JsonSerializer.Css
+			},
+
+			stateProperties:{
+				source:{value:''},
+				indentChars:{value:'\t'},
+				quoteChar:{value:'\''},
+				keyDelimiter:{value:':'},
+				padKeys:{value:false},
+				sortKeys:{value:false},
+
+				/*** derived properties ***/
+					result:{
+						derived:{
+							properties:['source','indentChars','quoteChar','keyDelimiter','padKeys','sortKeys'],
+							derivation:function (_source,_indentChars,_quoteChar,_keyDelimiter,_padKeys,_sortKeys) {
+								try {
+									return (
+										_source === ''
+											? ''
+											: Uize.Json.to (
+												Uize.Json.from (_source),
+												{
+													indentChars:_indentChars,
+													quoteChar:_quoteChar,
+													keyDelimiter:_keyDelimiter,
+													padKeys:_padKeys,
+													sortKeys:_sortKeys
+												}
+											)
+									);
+								} catch (_error) {
+									// do nothing
+								}
+							}
+						}
+					}
+			},
+
+			children:{
+				sourceVsResult:{widgetClass:Uize.Widgets.Tools.SourceVsResult.Widget},
+				padKeys:_onOffTogglerWidgetProperties,
+				sortKeys:_onOffTogglerWidgetProperties
+			},
+
+			eventBindings:{
+				'#indentChars:keyup':function (_event) {
+					this.set ({indentChars:_event.target.value});
+				},
+				'#quoteChar:change':function (_event) {
+					this.set ({quoteChar:_event.target.value});
+				},
+				'#keyDelimiter:keyup':function (_event) {
+					this.set ({keyDelimiter:_event.target.value});
+				}
+			},
+
+			childBindings:{
+				loc_sourceViewButtonLabel:'->sourceVsResult.sourceViewButtonLabel',
+				loc_resultViewButtonLabel:'->sourceVsResult.resultViewButtonLabel',
+				source:'sourceVsResult.source',
+				result:'->sourceVsResult.result',
+				padKeys:'padKeys.selected',
+				sortKeys:'sortKeys.selected'
+			},
+
+			htmlBindings:{
+				loc_presetsLabel:'presetsLabel',
+				loc_indentCharsLabel:'indentCharsLabel',
+				indentChars:'indentChars',
+				loc_linebreakCharsLabel:'linebreakCharsLabel',
+				loc_quoteCharLabel:'quoteCharLabel',
+				quoteChar:'quoteChar',
+				loc_keyDelimiterLabel:'keyDelimiterLabel',
+				keyDelimiter:'keyDelimiter',
+				loc_padKeysLabel:'padKeysLabel',
+				loc_keyAlignLabel:'keyAlignLabel',
+				loc_whenToQuoteKeysLabel:'whenToQuoteKeysLabel',
+				loc_sortKeysLabel:'sortKeysLabel'
+			}
+		});
+	}
+});
+
