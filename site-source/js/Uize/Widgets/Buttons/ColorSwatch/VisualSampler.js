8dc0fce04eb8bf71b3c3b44d8191945382b010db
Chris van Rensburg <git-uize@tomkidding.com>
2015-01-12 22:02:08 -0800

    Created a color swatch button (this will be used for a color picker form field widget).

--- site-source/js/Uize/Widgets/Buttons/ColorSwatch/VisualSampler.js
+++ site-source/js/Uize/Widgets/Buttons/ColorSwatch/VisualSampler.js
@@ -1,10 +1,10 @@
 /*______________
 |       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
 |     /      /  |   ---------------------------------------------------
-|    /    O /   |    MODULE : Uize.Widgets.ColorSwatch.VisualSampler Class
+|    /    O /   |    MODULE : Uize.Widgets.Buttons.ColorSwatch.VisualSampler Class
 |   /    / /    |
 |  /    / /  /| |    ONLINE : http://www.uize.com
-| /____/ /__/_| | COPYRIGHT : (c)2013-2014 UIZE
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
 |          /___ |   LICENSE : Available under MIT License or GNU General Public License
 |_______________|             http://www.uize.com/license.html
 */
@@ -12,22 +12,22 @@
 /* Module Meta Data
 	type: Class
 	importance: 1
-	codeCompleteness: 100
+	codeCompleteness: 5
 	docCompleteness: 100
 */
 
 /*?
 	Introduction
-		The =Uize.Widgets.ColorSwatch.VisualSampler= class implements a visual sampler widget for the =Uize.Widgets.ColorSwatch.Widget= class.
+		The =Uize.Widgets.Buttons.ColorSwatch.VisualSampler= class implements a visual sampler widget for the =Uize.Widgets.Buttons.ColorSwatch.Widget= class.
 
-		*DEVELOPERS:* `Chris van Rensburg`
+		*DEVELOPERS:*
 */
 
 Uize.module ({
-	name:'Uize.Widgets.ColorSwatch.VisualSampler',
+	name:'Uize.Widgets.Buttons.ColorSwatch.VisualSampler',
 	superclass:'Uize.Widgets.VisualSampler.Widget',
 	required:[
-		'Uize.Widgets.ColorSwatch.Widget',
+		'Uize.Widgets.Buttons.ColorSwatch.Widget',
 		'Uize.Widgets.StateValues'
 	],
 	builder:function (_superclass) {
@@ -36,15 +36,19 @@ Uize.module ({
 		return _superclass.subclass ({
 			omegastructor:function () {
 				this.addStateCombinationSamples ({
+					flavor:['normal','subdued'],
 					size:Uize.Widgets.StateValues.size,
+					value:'fc0'
+				});
+				this.addStateCombinationSamples ({
 					value:[
-						'red','green','blue','#f0f','#5f9ea0','#556b2f','#ffdead','#90ee90','#e99975','#4c7cd1','#000','#fff','#888'
+						'red','green','blue','#f0f','#5f9ea0','#556b2f','#ffdead','#90ee90','#e99975','#000','#fff','#888'
 					]
 				});
 			},
 
 			set:{
-				samplerWidgetClass:Uize.Widgets.ColorSwatch.Widget
+				samplerWidgetClass:Uize.Widgets.Buttons.ColorSwatch.Widget
 			}
 		});
 	}
---
+++ site-source/js/Uize/Widgets/Buttons/ColorSwatch/Widget.js
@@ -0,0 +1,70 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Buttons.ColorSwatch.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 5
+	docCompleteness: 5
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Buttons.ColorSwatch.Widget= module implements a widget class.
+
+		*DEVELOPERS:*
+
+		Visual Sampler
+			Below is a visual sampler of the =Uize.Widgets.Buttons.ColorSwatch.Widget= class...
+
+			...........................................................
+			<< widget >>
+
+			widgetClass: Uize.Widgets.Buttons.ColorSwatch.VisualSampler
+			...........................................................
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Buttons.ColorSwatch.Widget',
+	superclass:'Uize.Widgets.Button.Square.Widget',
+	required:[
+		'Uize.Widgets.Buttons.ColorSwatch.Html',
+		'Uize.Widgets.Buttons.ColorSwatch.Css',
+		'Uize.Color'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			set:{
+				html:Uize.Widgets.Buttons.ColorSwatch.Html
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.Buttons.ColorSwatch.Css
+			},
+
+			stateProperties:{
+				value:{value:'000'},
+
+				/*** derived properties ***/
+					valueAsRgbHex:{
+						derived:'value: Uize.Color.from (value).to ("#hex")'
+					}
+			},
+
+			htmlBindings:{
+				valueAsRgbHex:'swatch:style.backgroundColor'
+			}
+		});
+	}
+});
+
