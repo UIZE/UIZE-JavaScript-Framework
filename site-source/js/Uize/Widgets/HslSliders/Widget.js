c94ae516874e2ced66c9d42c89f9428d0e2c31ea
Chris van Rensburg <git-uize@tomkidding.com>
2015-01-19 21:17:30 -0800

    Created a widget to combine the hue, saturation, and lightness sliders.

---
+++ site-source/js/Uize/Widgets/HslSliders/Widget.js
@@ -0,0 +1,94 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.HslSliders.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 5
+	docCompleteness: 5
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.HslSliders.Widget= module implements a widget class.
+
+		*DEVELOPERS:*
+
+		Visual Sampler
+			Below is a visual sampler of the =Uize.Widgets.HslSliders.Widget= class...
+
+			..................................................
+			<< widget >>
+
+			widgetClass: Uize.Widgets.HslSliders.VisualSampler
+			..................................................
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.HslSliders.Widget',
+	superclass:'Uize.Widget.V2',
+	required:[
+		'Uize.Widgets.HslSliders.Html',
+		'Uize.Widgets.HslSliders.Css',
+		'Uize.Widgets.HslSliders.HueSlider.Widget',
+		'Uize.Widgets.HslSliders.SaturationSlider.Widget',
+		'Uize.Widgets.HslSliders.LightnessSlider.Widget'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			hasLoc:true,
+
+			set:{
+				html:Uize.Widgets.HslSliders.Html
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.HslSliders.Css
+			},
+
+			stateProperties:{
+				hue:{value:0},
+				saturation:{value:100},
+				lightness:{value:50}
+			},
+
+			children:{
+				hue:Uize.Widgets.HslSliders.HueSlider.Widget,
+				saturation:Uize.Widgets.HslSliders.SaturationSlider.Widget,
+				lightness:Uize.Widgets.HslSliders.LightnessSlider.Widget
+			},
+
+			childBindings:{
+				hue:[
+					'hue.value',
+					'saturation.hue',
+					'lightness.hue'
+				],
+				saturation:[
+					'saturation.value',
+					'lightness.saturation'
+				],
+				lightness:[
+					'lightness.value'
+				]
+			},
+
+			htmlBindings:{
+				loc_hueLabel:'hueLabel',
+				loc_saturationLabel:'saturationLabel',
+				loc_lightnessLabel:'lightnessLabel'
+			}
+		});
+	}
+});
+
