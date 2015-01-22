4fb740a4ef7f63650d0794ddc1d940c4126aa7d9
Chris van Rensburg <git-uize@tomkidding.com>
2015-01-22 10:43:56 -0800

    Created a combo RGB + HSL color sliders widget, with switchable mode.

---
+++ site-source/js/Uize/Widgets/ColorSliders/Vert/Combo/Widget.js
@@ -0,0 +1,142 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.ColorSliders.Vert.Combo.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 5
+	docCompleteness: 5
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.ColorSliders.Vert.Combo.Widget= module implements a widget class.
+
+		*DEVELOPERS:*
+
+		Visual Sampler
+			Below is a visual sampler of the =Uize.Widgets.ColorSliders.Vert.Combo.Widget= class...
+
+			...............................................................
+			<< widget >>
+
+			widgetClass: Uize.Widgets.ColorSliders.Vert.Combo.VisualSampler
+			...............................................................
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.ColorSliders.Vert.Combo.Widget',
+	superclass:'Uize.Widget.V2',
+	required:[
+		'Uize.Widgets.ColorSliders.Vert.Combo.Html',
+		'Uize.Widgets.ColorSliders.Vert.Combo.Css',
+		'Uize.Widgets.Button.Widget',
+		'Uize.Widgets.ColorSliders.Vert.Rgb.Widget',
+		'Uize.Widgets.ColorSliders.Vert.Hsl.Widget',
+		'Uize.Color',
+		'Uize.Util.PropertyAdapter'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		var
+			/*** General Variables ***/
+				_modes = ['rgb','hsl']
+		;
+
+		return _superclass.subclass ({
+			hasLoc:true,
+
+			alphastructor:function () {
+				var m = this;
+
+				Uize.forEach (
+					_modes,
+					function (_mode) {
+						m.addedChildren.once (
+							_mode,
+							function () {
+								Uize.Util.PropertyAdapter ({
+									propertyA:m,
+									propertyB:m.children [_mode]
+								});
+							}
+						);
+					}
+				);
+			},
+
+			set:{
+				html:Uize.Widgets.ColorSliders.Vert.Combo.Html
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.ColorSliders.Vert.Combo.Css
+			},
+
+			staticMethods:{
+				modes:function (_modes) {
+					var m = this;
+					Uize.forEach (
+						_modes,
+						function (_mode) {
+							var
+								_modeSelectedProperty = _mode + 'Selected',
+								_modeButtonName = _mode + 'Button'
+							;
+							m.declare ({
+								children:Uize.pairUp (
+									_mode,{},
+									_modeButtonName,{
+										widgetClass:Uize.Widgets.Button.Widget,
+										action:function () {this.parent.set ({mode:_mode})}
+									}
+								),
+								stateProperties:Uize.pairUp (
+									_modeSelectedProperty,{derived:'mode: mode == "' + _mode + '"'}
+								),
+								childBindings:Uize.pairUp (
+									_modeSelectedProperty,'->' + _modeButtonName + '.selected',
+									'loc_' + _mode,'->' + _modeButtonName + '.text',
+									'sliderHeight','->' + _mode + '.sliderHeight'
+								),
+								htmlBindings:Uize.pairUp (_modeSelectedProperty,_mode + ':?')
+							});
+						}
+					);
+				}
+			},
+
+			modes:_modes,
+
+			stateProperties:{
+				mode:{value:_modes [0]},
+				value:{value:'000'},
+				sliderHeight:{value:250},
+
+				/*** derived properties ***/
+					valueAsRgbHex:{
+						derived:'value: Uize.Color.from (value).to ("#hex")'
+					}
+			},
+
+			children:{
+				rgb:{widgetClass:Uize.Widgets.ColorSliders.Vert.Rgb.Widget},
+				hsl:{widgetClass:Uize.Widgets.ColorSliders.Vert.Hsl.Widget}
+			},
+
+			htmlBindings:{
+				valueAsRgbHex:'swatch:style.backgroundColor'
+			}
+		});
+	}
+});
+
