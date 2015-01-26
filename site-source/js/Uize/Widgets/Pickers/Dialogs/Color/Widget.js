73c6e30247c524171cefc639573fdb4e1f1d333c
Chris van Rensburg <git-uize@tomkidding.com>
2015-01-25 19:32:30 -0800

    Wrapped the vertical combo color sliders widget in a picker dialog to make a color picker dialog.

---
+++ site-source/js/Uize/Widgets/Pickers/Dialogs/Color/Widget.js
@@ -0,0 +1,42 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Pickers.Dialogs.Color.Widget
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 4
+	codeCompleteness: 100
+	docCompleteness: 80
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Pickers.Dialogs.Color.Widget= widget lets the user select a color from a modal dialog containing a color picker widget.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Pickers.Dialogs.Color.Widget',
+	required:'Uize.Widgets.ColorSliders.Vert.Combo.Widget',
+	superclass:'Uize.Widgets.Dialog.Picker.Widget',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			set:{
+				title:'Pick a color',
+				keepOpen:true,
+				valueWidgetClass:Uize.Widgets.ColorSliders.Vert.Combo.Widget
+			}
+		});
+	}
+});
+
