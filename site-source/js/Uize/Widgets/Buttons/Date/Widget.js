c4ba8d379cf027ece6dffa4acb6dea60efb8e6ee
Chris van Rensburg <git-uize@tomkidding.com>
2015-01-25 21:39:54 -0800

    Created a crude data button for the date picker widget.

---
+++ site-source/js/Uize/Widgets/Buttons/Date/Widget.js
@@ -0,0 +1,56 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Buttons.Date.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 1
+	codeCompleteness: 5
+	docCompleteness: 5
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Buttons.Date.Widget= module implements a widget class.
+
+		*DEVELOPERS:*
+
+		Visual Sampler
+			Below is a visual sampler of the =Uize.Widgets.Buttons.Date.Widget= class...
+
+			....................................................
+			<< widget >>
+
+			widgetClass: Uize.Widgets.Buttons.Date.VisualSampler
+			....................................................
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Buttons.Date.Widget',
+	superclass:'Uize.Widgets.Button.Square.Widget',
+	required:[
+		'Uize.Widgets.Buttons.Date.Html',
+		'Uize.Widgets.Buttons.Date.Css'
+	],
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			set:{
+				html:Uize.Widgets.Buttons.Date.Html
+			},
+
+			staticProperties:{
+				cssModule:Uize.Widgets.Buttons.Date.Css
+			}
+		});
+	}
+});
+
