74bc191bd17d3c36ed554d738185fb7deefa3ac1
Chris van Rensburg <git-uize@tomkidding.com>
2015-03-03 10:34:22 -0800

    Need to cloak this for older JS interpreters.

--- site-source/js/Uize/Widget/HtmltCompiler.js
+++ site-source/js/Uize/Widget/HtmltCompiler.js
@@ -349,7 +349,7 @@ Uize.module ({
 												var _childName = _attributesLookup.name;
 
 											/*** special handling for the extraClasses (or class) property ***/
-												var _extraClasses = _attributesLookup.extraClasses || _attributesLookup.class;
+												var _extraClasses = _attributesLookup.extraClasses || _attributesLookup ['class'];
 												delete _attributesLookup.class;
 												if (_extraClasses) {
 													_extraClasses = Uize.map (
