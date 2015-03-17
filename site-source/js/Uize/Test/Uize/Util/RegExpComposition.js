2a7e6f5642667ef6372d2d19938c92e597827945
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2015-03-17 16:29:37 -0700

    Some basic unit test cases for the Uize.Util.RegExpComposition object.

--- site-source/js/Uize/Test/Uize/Util/RegExpComposition.js
+++ site-source/js/Uize/Test/Uize/Util/RegExpComposition.js
@@ -29,30 +29,75 @@ Uize.module ({
 	builder:function () {
 		'use strict';
 
-		function _getIdentifierRegExpComposition () {
-			return Uize.Util.RegExpComposition ({
-				startCharacter:/[a-zA-Z_$]/,
-				continuationCharacter:/({startCharacter}|\d)/,
-				identifier:/^{startCharacter}{continuationCharacter}*$/
-			});
-		}
-
 		return Uize.Test.resolve ({
 			title:'Test for Uize.Util.RegExpComposition Module',
 			test:[
 				Uize.Test.requiredModulesTest ('Uize.Util.RegExpComposition'),
 				{
-					title:'',
+					title:'A regular expression composition object may contain a component that is simply a regular expression, and the g (global) flag will be set when it is resolved to a regular expression',
 					test:function () {
-						var _identifierRegExp = _getIdentifierRegExpComposition ().get ('identifier');
-						return this.expect (true,_identifierRegExp.test ('aABC_$'));
+						var _regExpComposition = Uize.Util.RegExpComposition ({
+							digits:/\d+/
+						});
+						return this.expect (/\d+/g,_regExpComposition.get ('digits'));
 					}
 				},
 				{
-					title:'',
+					title:'A regular expression composition object may contain a component that references another component',
 					test:function () {
-						var _identifierRegExp = _getIdentifierRegExpComposition ().get ('identifier');
-						return this.expect (false,_identifierRegExp.test ('0ABC_$'));
+						var _regExpComposition = Uize.Util.RegExpComposition ({
+							digit:/\d/,
+							digits:/{digit}+/
+						});
+						return this.expect (/\d+/g,_regExpComposition.get ('digits'));
+					}
+				},
+				{
+					title:'A regular expression composition object may contain a component that references multiple other components',
+					test:function () {
+						var _regExpComposition = Uize.Util.RegExpComposition ({
+							startCharacter:/[a-zA-Z_$]/,
+							continuationCharacter:/({startCharacter}|\d)/,
+							identifier:/^{startCharacter}{continuationCharacter}*$/
+						});
+						return this.expect (/^[a-zA-Z_$]([a-zA-Z_$]|\d)*$/g,_regExpComposition.get ('identifier'));
+					}
+				},
+				{
+					title:'When a regular expression composition object contain multiple components, each component can be resolved to a regular expression',
+					test:function () {
+						var _regExpComposition = Uize.Util.RegExpComposition ({
+							startCharacter:/[a-zA-Z_$]/,
+							continuationCharacter:/({startCharacter}|\d)/,
+							identifier:/^{startCharacter}{continuationCharacter}*$/
+						});
+						return (
+							this.expect (/[a-zA-Z_$]/g,_regExpComposition.get ('startCharacter')) &&
+							this.expect (/([a-zA-Z_$]|\d)/g,_regExpComposition.get ('continuationCharacter')) &&
+							this.expect (/^[a-zA-Z_$]([a-zA-Z_$]|\d)*$/g,_regExpComposition.get ('identifier'))
+						);
+					}
+				},
+				{
+					title:'Names of components of a regular expression composition object may begin with a "$" (dollar) character or a "_" (underscore) character',
+					test:function () {
+						var _regExpComposition = Uize.Util.RegExpComposition ({
+							$foo:/foo/,
+							_bar:/bar/,
+							fooBar:/{$foo}{_bar}/
+						});
+						return this.expect (/foobar/g,_regExpComposition.get ('fooBar'));
+					}
+				},
+				{
+					title:'Names of components of a regular expression composition object may not begin with a digit, but may contain digits, dollars, and underscores',
+					test:function () {
+						var _regExpComposition = Uize.Util.RegExpComposition ({
+							1:/one/,
+							a_$123456789_$:/bar/,
+							foo:/foo{1}{a_$123456789_$}/
+						});
+						return this.expect (/foo{1}bar/g,_regExpComposition.get ('foo'));
 					}
 				}
 			]
