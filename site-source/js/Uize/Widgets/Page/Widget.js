8e5e9a8e3efcb6e1372d85267bdd3dab0a62895f
Chris van Rensburg <git-uize@tomkidding.com>
2015-08-02 12:10:13 -0700

    Created a simple V2 page widget abstract base class.

---
+++ site-source/js/Uize/Widgets/Page/Widget.js
@@ -0,0 +1,38 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Widgets.Page.Widget Class
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Class
+	importance: 3
+	codeCompleteness: 100
+	docCompleteness: 5
+*/
+
+/*?
+	Introduction
+		The =Uize.Widgets.Page.Widget= class implements an abstract page widget base class.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Widgets.Page.Widget',
+	superclass:'Uize.Widget.Page',
+	required:'Uize.Widget.mV2',
+	builder:function (_superclass) {
+		'use strict';
+
+		return _superclass.subclass ({
+			mixins:Uize.Widget.mV2
+		});
+	}
+});
+
