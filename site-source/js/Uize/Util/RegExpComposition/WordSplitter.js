710339bfbfd9ba28246f207d6c6ab4b6d84c9fc2
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2015-10-02 18:07:21 -0700

    Created two simple word splitter RegExpComposition objects - one basic one, and one with support for HTML tags and entities.

---
+++ site-source/js/Uize/Util/RegExpComposition/WordSplitter.js
@@ -0,0 +1,40 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Util.RegExpComposition.WordSplitter Object
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Object
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 4
+*/
+
+/*?
+	Introduction
+		The =Uize.Util.RegExpComposition.WordSpliiter= package defines a regular expression composition object for matching non-word character sequences.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Util.RegExpComposition.WordSplitter',
+	required:'Uize.Util.RegExpComposition',
+	builder:function () {
+		'use strict';
+
+		return Uize.Util.RegExpComposition ({
+			punctuation:/[\?!\.;,&=\-\(\)\[\]"<>]+/,
+			number:/\d+(?:\.\d+)?/,
+			whitespace:/\s+/,
+			wordSplitter:/({whitespace}|{punctuation}|{number})/
+		});
+	}
+});
+
---
+++ site-source/js/Uize/Util/RegExpComposition/WordSplitterHtml.js
@@ -0,0 +1,42 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Util.RegExpComposition.WordSplitterHtml Object
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Object
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 4
+*/
+
+/*?
+	Introduction
+		The =Uize.Util.RegExpComposition.WordSpliiterHtml= package defines a regular expression composition object for matching non-word character sequences in strings that contains HTML tags and HTML entities.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Util.RegExpComposition.WordSplitterHtml',
+	required:[
+		'Uize.Util.RegExpComposition.WordSpliiter',
+		'Uize.Util.Html.Encode'
+	],
+	builder:function () {
+		'use strict';
+
+		return Uize.Util.RegExpComposition.WordSpliiter.extend ({
+			htmlEntity:Uize.Util.Html.Encode.entityRegExp,
+			htmlTag:/<(?:.|[\r\n\f])+?>/,
+			wordSplitter:/({htmlTag}|{htmlEntity}|{whitespace}|{punctuation}|{number})/
+		});
+	}
+});
+
