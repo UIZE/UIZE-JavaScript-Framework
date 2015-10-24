60a6fd9e9c37e58fc2feea84349f66e3b2388978
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2015-10-23 17:33:47 -0700

    Created a loc file format parser and serializer package for Java properties files where characters outside the ASCII range must be Unicode-escaped.

---
+++ site-source/js/Uize/Loc/FileFormats/JavaPropertiesAscii.js
@@ -0,0 +1,46 @@
+/*______________
+|       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
+|     /      /  |   ---------------------------------------------------
+|    /    O /   |    MODULE : Uize.Loc.FileFormats.JavaPropertiesAscii Package
+|   /    / /    |
+|  /    / /  /| |    ONLINE : http://www.uize.com
+| /____/ /__/_| | COPYRIGHT : (c)2015 UIZE
+|          /___ |   LICENSE : Available under MIT License or GNU General Public License
+|_______________|             http://www.uize.com/license.html
+*/
+
+/* Module Meta Data
+	type: Package
+	importance: 1
+	codeCompleteness: 100
+	docCompleteness: 50
+*/
+
+/*?
+	Introduction
+		The =Uize.Loc.FileFormats.JavaPropertiesAscii= module provides support for serializing to and parsing from [[http://en.wikipedia.org/wiki/.properties][Java properties]] files, where all characters outside the ASCII range should be Unicode-escaped.
+
+		*DEVELOPERS:* `Chris van Rensburg`
+*/
+
+Uize.module ({
+	name:'Uize.Loc.FileFormats.JavaPropertiesAscii',
+	required:[
+		'Uize.Loc.FileFormats.JavaProperties',
+		'Uize.Parse.JavaPropertiesAscii.Document',
+		'Uize.Parse.JavaPropertiesAscii.Property'
+	],
+	builder:function () {
+		'use strict';
+
+		return Uize.copyInto (
+			Uize.package (),
+			Uize.Loc.FileFormats.JavaProperties,
+			{
+				documentParser:Uize.Parse.JavaPropertiesAscii.Document,
+				propertyParser:Uize.Parse.JavaPropertiesAscii.Property
+			}
+		);
+	}
+});
+
