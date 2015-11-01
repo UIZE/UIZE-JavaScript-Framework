635cee0fc83553c35d5d2cb63561b6063bdfd384
Chris Van Rensburg <chris.vanrensburg@ringcentral.com>
2015-11-01 10:52:08 -0800

    Turned the Uize.Util.RegExpComposition module into a true class module.

--- site-source/js/Uize/Util/RegExpComposition.js
+++ site-source/js/Uize/Util/RegExpComposition.js
@@ -1,7 +1,7 @@
 /*______________
 |       ______  |   U I Z E    J A V A S C R I P T    F R A M E W O R K
 |     /      /  |   ---------------------------------------------------
-|    /    O /   |    MODULE : Uize.Util.RegExpComposition Package
+|    /    O /   |    MODULE : Uize.Util.RegExpComposition Class
 |   /    / /    |
 |  /    / /  /| |    ONLINE : http://www.uize.com
 | /____/ /__/_| | COPYRIGHT : (c)2014-2015 UIZE
@@ -10,7 +10,7 @@
 */
 
 /* Module Meta Data
-	type: Package
+	type: Class
 	importance: 1
 	codeCompleteness: 5
 	docCompleteness: 5
@@ -25,51 +25,49 @@
 
 Uize.module ({
 	name:'Uize.Util.RegExpComposition',
-	builder:function () {
+	superclass:'Uize.Oop.BasicClass',
+	builder:function (_superclass) {
 		'use strict';
 
-		var _constructor = Uize.mergeInto (
-			Uize.noNew (
+		return _superclass.subclass ({
+			constructor:Uize.noNew (
 				function (_regExpLookup) {
 					this._regExpLookup = _regExpLookup;
 					this._resolved = {};
 				}
 			),
-			{
-				prototype:{
-					get:function (_name) {
-						var
-							_regExpLookup = this._regExpLookup,
-							_resolved = this._resolved
-						;
-						function _resolveRegularExpression (_regExpName) {
-							if (!_resolved [_regExpName]) {
-								var _regExp = _regExpLookup [_regExpName];
-								_resolved [_regExpName] = new RegExp (
-									_regExp.source.replace (
-										/{([a-zA-Z_$][a-zA-Z_$\d]*)}/g,
-										function (_match,_subRegExpName) {
-											_resolveRegularExpression (_subRegExpName);
-											return _resolved [_subRegExpName].source;
-										}
-									),
-									'g'
-								)
-							}
+
+			instanceMethods:{
+				get:function (_name) {
+					var
+						_regExpLookup = this._regExpLookup,
+						_resolved = this._resolved
+					;
+					function _resolveRegularExpression (_regExpName) {
+						if (!_resolved [_regExpName]) {
+							var _regExp = _regExpLookup [_regExpName];
+							_resolved [_regExpName] = new RegExp (
+								_regExp.source.replace (
+									/{([a-zA-Z_$][a-zA-Z_$\d]*)}/g,
+									function (_match,_subRegExpName) {
+										_resolveRegularExpression (_subRegExpName);
+										return _resolved [_subRegExpName].source;
+									}
+								),
+								'g'
+							)
 						}
-						_resolveRegularExpression (_name);
-
-						return _resolved [_name];
-					},
-
-					extend:function (_regExpLookup) {
-						return _constructor (Uize.copy (this._regExpLookup,_regExpLookup))
 					}
+					_resolveRegularExpression (_name);
+
+					return _resolved [_name];
+				},
+
+				extend:function (_regExpLookup) {
+					return this.Class (Uize.copy (this._regExpLookup,_regExpLookup))
 				}
 			}
-		);
-
-		return _constructor;
+		});
 	}
 });
 
